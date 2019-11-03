423f59e913feae328ab9313392ea6d4ce7852c63
José Mariano Gonçalves <marianogoncalves8@gmail.com>
2019-11-03 14:27:06 +0100

    Class Provincia e Monicipio from criados no pacote model

---
+++ src/app/shared/model/monicipio.ts
@@ -0,0 +1,16 @@
+import { CustomAbstractEntity } from 'src/app/shared/model/customEntity';
+import { Admin } from './admin';
+
+export class Monicipio implements CustomAbstractEntity {
+
+  id: number;
+  createdAt: string;
+  lastModifiedAt: string;
+  totalModified: number;
+  nome: string;
+  admin: Admin;
+
+  constructor(id?: number) {
+    this.id = id ? id : null;
+  }
+}
