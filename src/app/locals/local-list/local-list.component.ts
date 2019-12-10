decdd49eb5b2462b0bd5504baad4e4b1902a00f1
Mariano JavaSwing <marianogoncalves8@gmail.com>
2019-12-10 22:15:06 +0100

    listando todos os locais na tabela. LocaisListComponent

--- src/app/locals/local-list/local-list.component.ts
+++ src/app/locals/local-list/local-list.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog, MatSort, MatTableDataSource } from '@angular/material';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of, Subject, Subscription } from 'rxjs';
+import { catchError } from 'rxjs/internal/operators/catchError';
+import { MonografiaService } from 'src/app/monografias/modules/monografia.service';
+import { DeleteDialogComponent } from 'src/app/shared/delete-dialog/delete-dialog.component';
+import { ErrorLoadingComponent } from 'src/app/shared/error-loading/error-loading.component';
+import { CustomFilter } from 'src/app/shared/model/support/custom-filter';
+import { MatDailogTypeParam } from 'src/app/shared/model/support/mat-dialog-type-param';
+import { MoreOptionsDialogComponent } from 'src/app/shared/more-options-dialog/more-options-dialog.component';
+import { NotificationService } from 'src/app/shared/services/notification/notification.service';
+import { Local } from 'src/app/shared/model/local';
+import { LocalService } from '../modules/local.service';
 
 @Component({
   selector: 'app-local-list',
@@ -7,9 +20,162 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocalListComponent implements OnInit {
 
-  constructor() { }
+  dialogParam: MatDailogTypeParam = new MatDailogTypeParam();
+  valueParam = '';
+  filtro: CustomFilter = new CustomFilter();
+  private sub: Subscription;
+  locais: MatTableDataSource<Local>;
+  locaisList: Local[] = [];
+  error$ = new Subject<boolean>();
 
-  ngOnInit() {
+
+
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+
+  displaydColumns: string[] = [
+    'destrito',
+    'duracao',
+    'departamento',
+    'detalhe',
+    'edit',
+    'delete'
+  ];
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    public service: LocalService,
+    public monografiaService: MonografiaService,
+    private notification: NotificationService,
+    private dialogService: MatDialog) {
+    this.monografiaService.emitShowAddButton.emit(true);
   }
 
+  ngOnInit() {
+    this.service.onChangeContext.emit(false);
+
+    this.sub = this.service.findValueParams
+      .subscribe(next => this.onRefrash(next));
+
+    this.sub = this.service.findValueParam
+      .subscribe(next => this.locais.filter = next);
+
+    this.onRefrash(this.filtro);
+
+    this.sub = this.service.emitOnDetalheButtonCliked.subscribe(
+      (next) => this.detalhe(next)
+    );
+
+    this.sub = this.service.emitOnEditButtonCliked.subscribe(
+      (next) => this.edit(next)
+    );
+
+    this.sub = this.service.emitOnDeleteButtonCliked.subscribe(
+      (next) => this.openDeleteDialog(next)
+    );
+
+    this.sub = this.service.findValueParamFromServer.subscribe(
+      (next: CustomFilter) => this.onFilterFromServer(next)
+    );
+  }
+
+  onFilterFromServer(data: CustomFilter) {
+    this.sub = this.service.filterByNome(data).subscribe(
+      (next: Local[]) => this.locaisList = next
+    );
+  }
+
+  onRefrash(data?: CustomFilter) {
+    this.sub = this.service.filterByNome(data)
+      .pipe(
+        catchError(err => {
+          console.log(err);
+
+          this.dialogService.open(ErrorLoadingComponent);
+          this.error$.next(true);
+          return of(null);
+        })
+      )
+      .subscribe(
+        next => {
+          const array = next.map((item: Local) => {
+            return {
+              ...item
+            };
+
+          });
+          this.locais = new MatTableDataSource(array);
+          this.locais.sort = this.sort;
+          this.locaisList = this.locais.data;
+        });
+  }
+
+  private showErrorMessage() {
+    this.notification.componentLoadingFailedMessage();
+  }
+  private showDeletedMessage() {
+    this.notification.componentDeletetedSuccessfulMessage();
+  }
+
+  add() {
+    this.router.navigate(['add'], { relativeTo: this.activatedRoute });
+  }
+
+  detalhe(id: number) {
+    this.router.navigate(['detalhe', id], { relativeTo: this.activatedRoute });
+  }
+  edit(id: number) {
+    this.router.navigate(['edit', id], { relativeTo: this.activatedRoute });
+  }
+
+  OnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  openMoreOptionDialog(id: number) {
+
+    this.dialogParam.id = id;
+    this.dialogParam.entityName = 'Local';
+
+    const dialogRef = this.dialogService.open(
+      MoreOptionsDialogComponent,
+      {
+        data: this.dialogParam,
+        height: '280px',
+        width: '360px'
+      });
+
+    dialogRef.afterClosed().subscribe((result: boolean) => {
+      if (result) {
+      }
+    });
+  }
+
+  openDeleteDialog(id: number) {
+    const dialogRef = this.dialogService.open(
+      DeleteDialogComponent,
+      {
+        data: id,
+        height: '200px',
+        width: '360px'
+      });
+
+    dialogRef.afterClosed().subscribe((result: boolean) => {
+      if (result) {
+        this.deleteLocal(id);
+      }
+
+    });
+  }
+
+  deleteLocal(cursoId: number) {
+    this.service.deleteById(cursoId)
+      .subscribe(
+        () => {
+          this.onRefrash(this.filtro);
+          this.showDeletedMessage();
+        },
+        err => this.showErrorMessage()
+      );
+  }
 }
--- src/app/locals/modules/local.service.ts
+++ src/app/locals/modules/local.service.ts
@@ -1,8 +1,53 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import { CustomFilter } from 'src/app/shared/model/support/custom-filter';
+import { Local } from 'src/app/shared/model/local';
+import { CustomRepository } from 'src/app/shared/repository/custom-repository';
+import { CrudService } from 'src/app/shared/services/crud/crud.service';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
-export class LocalService {
+export class LocalService implements CustomRepository<Local, number> {
 
+  findValueParam = new EventEmitter<string>();
+  onChangeContextTitle = new EventEmitter<string>();
+  findValueParamFromServer = new EventEmitter<CustomFilter>();
+  findValueParams = new EventEmitter<CustomFilter>();
+  onChangeContext = new EventEmitter<boolean>();
+  emitOnDetalheButtonCliked = new EventEmitter<number>();
+  emitOnEditButtonCliked = new EventEmitter<number>();
+  emitOnDeleteButtonCliked = new EventEmitter<number>();
+
+ constructor(private service: CrudService<Local, number>) { }
+
+  getById(id: number): Observable<Local> {
+    return this.service.getById('interno/local', id);
+  }
+
+  list(): Observable<Local[]> {
+    return this.service.list('local');
+  }
+
+  filterByNome(filter: CustomFilter): Observable<Local[]> {
+    return this.service.list(`local?destrito=${!!filter.nome ? filter.nome : ''}`)
+      ;
+  }
+
+  filterByDuracao(duracao: number): Observable<Local[]> {
+    return this.service
+      .list(`interno/local/l?duracao=${!!duracao ? duracao : 1}`);
+  }
+
+
+  save(t: Local): Observable<Local> {
+    if (t.id) {
+      return this.service.update('interno/local', t);
+    }
+    return this.service.save('interno/local', t);
+  }
+
+  deleteById(id: number): Observable<void> {
+    return this.service.deleteById('interno/local', id);
+  }
 }
