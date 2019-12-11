f832f495faf6bd9c61dcd6c6b8e476ca1b737f4b
Mariano JavaSwing <marianogoncalves8@gmail.com>
2019-12-11 13:59:00 +0100

    Botao para voltar abilitado na pagina de detalhe do Instituto

--- src/app/institutos/instituto-detalhe/instituto-detalhe.component.ts
+++ src/app/institutos/instituto-detalhe/instituto-detalhe.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MonografiaService } from 'src/app/monografias/modules/monografia.service';
+import { InstitutoService } from '../modules/instituto.service';
 
 @Component({
   selector: 'app-instituto-detalhe',
@@ -8,11 +9,14 @@ import { MonografiaService } from 'src/app/monografias/modules/monografia.servic
 })
 export class InstitutoDetalheComponent implements OnInit {
 
-  constructor(private monografiaService: MonografiaService) {
+  constructor(
+    private institutoService: InstitutoService,
+    private monografiaService: MonografiaService) {
     this.monografiaService.emitShowAddButton.emit(true);
   }
 
   ngOnInit() {
+    this.institutoService.onChangeContext.emit(true);
   }
 
 }
