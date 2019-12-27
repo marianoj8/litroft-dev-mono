df09b9f3568df7c97856a7fe77a26f953a92d873
Mariano JavaSwing <marianogoncalves8@gmail.com>
2019-12-28 00:17:40 +0100

    Metodos do CRUD foram criados

--- src/app/municipio/modules/municipio.service.ts
+++ src/app/municipio/modules/municipio.service.ts
@@ -1,8 +1,53 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import { CrudService } from 'src/app/shared/services/crud/crud.service';
+import { CustomFilter } from 'src/app/shared/model/support/custom-filter';
+import { CustomRepository } from 'src/app/shared/repository/custom-repository';
+import { Observable } from 'rxjs';
+import { Municipio } from 'src/app/shared/model/monicipio';
 
 @Injectable({
   providedIn: 'root'
 })
-export class MunicipioService {
+export class MunicipioService implements CustomRepository<Municipio, number> {
 
+  findValueParam = new EventEmitter<string>();
+  onChangeContextTitle = new EventEmitter<string>();
+  findValueParamFromServer = new EventEmitter<CustomFilter>();
+  findValueParams = new EventEmitter<CustomFilter>();
+  onChangeContext = new EventEmitter<boolean>();
+  emitOnDetalheButtonCliked = new EventEmitter<number>();
+  emitOnEditButtonCliked = new EventEmitter<number>();
+  emitOnDeleteButtonCliked = new EventEmitter<number>();
+
+  constructor(private service: CrudService<Municipio, number>) { }
+
+  getById(id: number): Observable<Municipio> {
+    return this.service.getById('interno/municipio', id);
+  }
+
+  list(): Observable<Municipio[]> {
+    return this.service.list('municipio');
+  }
+
+  filterByNome(filter: CustomFilter): Observable<Municipio[]> {
+    return this.service.list(`municipio?destrito=${!!filter.nome ? filter.nome : ''}`)
+      ;
+  }
+
+  filterByDuracao(duracao: number): Observable<Municipio[]> {
+    return this.service
+      .list(`interno/municipio/l?duracao=${!!duracao ? duracao : 1}`);
+  }
+
+
+  save(t: Municipio): Observable<Municipio> {
+    if (t.id) {
+      return this.service.update('interno/municipio', t);
+    }
+    return this.service.save('interno/municipio', t);
+  }
+
+  deleteById(id: number): Observable<void> {
+    return this.service.deleteById('interno/municipio', id);
+  }
 }
