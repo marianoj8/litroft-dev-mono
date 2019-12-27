d59bceda980b325ec1053f76ed27bfe405d95784
Mariano JavaSwing <marianogoncalves8@gmail.com>
2019-12-27 17:50:11 +0100

    Modulo de funcionalidade para municipio foi criado

---
+++ src/app/municipio/modules/municipio.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MunicipioRoutingModule } from './municipio-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MunicipioRoutingModule
+  ],
+  exports: []
+})
+export class MunicipioModule {
+
+}
