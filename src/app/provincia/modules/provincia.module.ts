565c8a58ea89257c7f258bc83b9ad59d058d01e4
Mariano JavaSwing <marianogoncalves8@gmail.com>
2019-12-27 16:41:43 +0100

    Modulos necessarios form importados em imports:[]

--- src/app/provincia/modules/provincia.module.ts
+++ src/app/provincia/modules/provincia.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
+import { ProvinciaComponent } from '../provincia.component';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../../material/material.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ProvinciaRoutingModule } from './provincia-routing.module';
 
 @NgModule({
-  declarations: [],
-  imports: [],
+  declarations: [ProvinciaComponent],
+  imports: [
+    CommonModule,
+    ProvinciaRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MaterialModule
+  ],
   exports: []
 })
 export class ProvinciaModule {
