735c4219dfdb1ab9fa4f5891feeb813fb84c2056
Mariano JavaSwing <marianogoncalves8@gmail.com>
2019-12-29 11:35:43 +0100

    Rotas para casdastro e edicao de adminInterno (directores) adicionados

--- src/app/admin-interno/modules/adminInterno-routing.module.ts
+++ src/app/admin-interno/modules/adminInterno-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminInternoComponent } from '../admin-interno.component';
 import { AdminInternoListComponent } from '../admin-interno-list/admin-interno-list.component';
+import { AdminInternoFormComponent } from '../admin-interno-form/admin-interno-form.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,14 @@ const routes: Routes = [
       {
         path: '',
         component: AdminInternoListComponent
+      },
+      {
+        path: 'add',
+        component: AdminInternoFormComponent
+      },
+      {
+        path: 'edit/:id',
+        component: AdminInternoFormComponent
       }
     ]
   }
