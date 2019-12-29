e83a87db7e8403ea7245b4aeb92f5e17b19ba1d2
Mariano JavaSwing <marianogoncalves8@gmail.com>
2019-12-29 14:42:08 +0100

    imism-litroft foi renomeado para litroft-dev-mono

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/imism-litroft'),
+      dir: require('path').join(__dirname, './coverage/litroft-dev-mono'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
