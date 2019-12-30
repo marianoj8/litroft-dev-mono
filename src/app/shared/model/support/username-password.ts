894b28f63fc6a8e53c2b7f2ac6b35deaa254fbeb
Mariano JavaSwing <marianogoncalves8@gmail.com>
2019-12-30 14:43:46 +0100

    atributo password foi adicionado a class UsernameAndPassword

--- src/app/shared/model/support/username-password.ts
+++ src/app/shared/model/support/username-password.ts
@@ -1,6 +1,7 @@
 export class UsernameAndPassword {
   constructor(
     public username?: string,
-    public password?: string) {
+    public password?: string,
+    public serialNumber?: string) {
   }
 }
