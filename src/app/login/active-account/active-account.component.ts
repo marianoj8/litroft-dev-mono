afc09fd6c32d2d958ced464656fb347ab6b9f879
Mariano JavaSwing <marianogoncalves8@gmail.com>
2019-12-30 14:53:37 +0100

    formulario inicalizado e onSubmit foi criado

--- src/app/login/active-account/active-account.component.ts
+++ src/app/login/active-account/active-account.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { AuthService } from '../../shared/services/security/auth.service';
+import { UsernameAndPassword } from '../../shared/model/support/username-password';
 
 @Component({
   selector: 'app-active-account',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./active-account.component.css']
 })
 export class ActiveAccountComponent implements OnInit {
-
-  constructor() { }
+  activationForm: FormGroup;
+  error$ = new Subject<string>();
+  hide = true;
+  formBuilder = new FormBuilder();
+  user = new UsernameAndPassword();
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.activationForm = this.formBuilder.group({
+      serial: ['', [
+        Validators.minLength(8),
+        Validators.maxLength(12),
+        Validators.required
+      ]], username: ['', [
+        Validators.minLength(6),
+        Validators.maxLength(28),
+        Validators.required
+      ]], password: ['', [
+        Validators.minLength(8),
+        Validators.maxLength(20),
+        Validators.required
+      ]]
+    });
+  }
+
+  onSubmit() {
+    this.user.username = this.activationForm.controls.username.value;
+    this.user.password = this.activationForm.controls.password.value;
+    this.user.serialNumber = this.activationForm.controls.serial.value;
+    this.authService.activeAccount(this.user);
   }
 
 }
