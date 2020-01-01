86c70b6cdd2d59d99a0a17e79ef353e79bbefd05
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-01 03:49:09 +0100

    Refatoracao completa de projeto.service, impl removida

--- src/app/projetos/modules/projeto.service.ts
+++ src/app/projetos/modules/projeto.service.ts
@@ -1,14 +1,15 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 
-import { CrudService } from 'src/app/shared/services/crud/crud.service';
-import { CustomFilter } from 'src/app/shared/model/support/custom-filter';
 import { Projeto } from 'src/app/shared/model/projeto';
-import { CustomRepository } from 'src/app/shared/repository/custom-repository';
+import { CustomFilter } from 'src/app/shared/model/support/custom-filter';
+import { environment } from '../../../environments/environment.prod';
 
 @Injectable({ providedIn: 'root' })
-export class ProjetoService implements CustomRepository<Projeto, number> {
+export class ProjetoService {
 
+  url = environment.API;
   findValueParam = new EventEmitter<string>();
   onChangeContextTitle = new EventEmitter<string>();
   findValueParamFromServer = new EventEmitter<CustomFilter>();
@@ -18,34 +19,33 @@ export class ProjetoService implements CustomRepository<Projeto, number> {
   emitOnEditButtonCliked = new EventEmitter<number>();
   emitOnDeleteButtonCliked = new EventEmitter<number>();
 
-  constructor(private service: CrudService<Projeto, number>) { }
+  constructor(private http: HttpClient) { }
 
   getById(id: number): Observable<Projeto> {
-    return this.service.getById('interno/projeto', id);
+    return this.http.get<Projeto>(`${this.url}/interno/projeto/${id}`);
   }
 
   list(): Observable<Projeto[]> {
-    return this.service.list('interno/projeto/l?duracao=1');
+    return this.http.get<Projeto[]>(`${this.url}/interno/projeto/l?duracao=1`);
   }
 
-  filterByNomeDuracao(filter: CustomFilter): Observable<Projeto[]> {
-    return this.service.list(`interno/projeto/l?nome=${!!filter.nome ? filter.nome : ''}&duracao=${!!filter.duracao ? filter.duracao : 1}`);
+  filterByNomeDuracao(q: CustomFilter): Observable<Projeto[]> {
+    return this.http.get<Projeto[]>(`${this.url}/interno/projeto/l?nome=${!!q.nome ? q.nome : ''}&duracao=${!!q.duracao ? q.duracao : 1}`);
   }
 
   filterByDuracao(duracao: number): Observable<Projeto[]> {
-    return this.service
-      .list(`interno/projeto/l?duracao=${!!duracao ? duracao : 1}`);
+    return this.http
+      .get<Projeto[]>(`${this.url}/interno/projeto/l?duracao=${!!duracao ? duracao : 1}`);
   }
-
 
   save(t: Projeto): Observable<Projeto> {
     if (t.id) {
-      return this.service.update('interno/projeto', t);
+      return this.http.put<Projeto>(`${this.url}/interno/projeto`, t);
     }
-    return this.service.save('interno/projeto', t);
+    return this.http.post<Projeto>(`${this.url}/interno/projeto`, t);
   }
 
   deleteById(id: number): Observable<void> {
-    return this.service.deleteById('interno/projeto', id);
+    return this.http.delete<void>(`${this.url}/interno/projeto/${id}`);
   }
 }
