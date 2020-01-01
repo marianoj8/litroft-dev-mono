60f654e376ff91dc1e111781006926b7a34d3ee5
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-01 03:48:35 +0100

    Refatoracao completa de turma.service, impl removida

--- src/app/turmas/modules/turma.service.ts
+++ src/app/turmas/modules/turma.service.ts
@@ -1,16 +1,17 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-import { CrudService } from 'src/app/shared/services/crud/crud.service';
 import { CustomFilter } from 'src/app/shared/model/support/custom-filter';
 import { Turma } from 'src/app/shared/model/turma';
-import { CustomRepository } from 'src/app/shared/repository/custom-repository';
+import { environment } from '../../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TurmaService implements CustomRepository<Turma, number> {
+export class TurmaService {
 
+  url = environment.API;
   findValueParam = new EventEmitter<string>();
   onChangeContextTitle = new EventEmitter<string>();
   findValueParamFromServer = new EventEmitter<CustomFilter>();
@@ -20,32 +21,32 @@ export class TurmaService implements CustomRepository<Turma, number> {
   emitOnEditButtonCliked = new EventEmitter<number>();
   emitOnDeleteButtonCliked = new EventEmitter<number>();
 
-  constructor(private service: CrudService<Turma, number>) { }
+  constructor(private http: HttpClient) { }
 
   getById(id: number): Observable<Turma> {
-    return this.service.getById('interno/turma', id);
+    return this.http.get<Turma>(`${this.url}/interno/turma/${id}`);
   }
 
   list(): Observable<Turma[]> {
-    return this.service.list('interno/turma/l');
+    return this.http.get<Turma[]>(`${this.url}/interno/turma/l`);
   }
 
   filterByNome(filter: CustomFilter): Observable<Turma[]> {
-    return this.service.list(`interno/turma/l?sigla=${!!filter.sigla ? filter.sigla : ''}`);
+    return this.http.get<Turma[]>(`${this.url}/interno/turma/l?sigla=${!!filter.sigla ? filter.sigla : ''}`);
   }
 
   findByCurso(id: number): Observable<Turma[]> {
-    return this.service.list(`interno/turma/l/c?curso=${!!id ? id : ''}&nome=13`);
+    return this.http.get<Turma[]>(`${this.url}/interno/turma/l/c?curso=${!!id ? id : ''}&nome=13`);
   }
 
   save(t: Turma): Observable<Turma> {
     if (t.id) {
-      return this.service.update('interno/turma', t);
+      return this.http.put<Turma>(`${this.url}/interno/turma`, t);
     }
-    return this.service.save('interno/turma', t);
+    return this.http.post<Turma>(`${this.url}/interno/turma`, t);
   }
 
   deleteById(id: number): Observable<void> {
-    return this.service.deleteById('interno/turma', id);
+    return this.http.delete<void>(`${this.url}/interno/turma/${id}`);
   }
 }
