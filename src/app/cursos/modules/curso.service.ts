0e9f1d8eb425fd2964892afee62b10866f3f8bd1
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-01 02:52:07 +0100

    Refatoracao completa de curso.service, impl removida

--- src/app/cursos/modules/curso.service.ts
+++ src/app/cursos/modules/curso.service.ts
@@ -1,17 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { EventEmitter, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { Curso } from 'src/app/shared/model/curso';
 import { CustomFilter } from 'src/app/shared/model/support/custom-filter';
-import { CrudService } from 'src/app/shared/services/crud/crud.service';
-import { CustomRepository } from './../../shared/repository/custom-repository';
+import { environment } from '../../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CursoService implements CustomRepository<Curso, number> {
+export class CursoService {
 
-
+  url = environment.API;
   findValueParam = new EventEmitter<string>();
   onChangeContextTitle = new EventEmitter<string>();
   findValueParamFromServer = new EventEmitter<CustomFilter>();
@@ -21,35 +21,34 @@ export class CursoService implements CustomRepository<Curso, number> {
   emitOnEditButtonCliked = new EventEmitter<number>();
   emitOnDeleteButtonCliked = new EventEmitter<number>();
 
-  constructor(private service: CrudService<Curso, number>) { }
+  constructor(private http: HttpClient) { }
 
   getById(id: number): Observable<Curso> {
-    return this.service.getById('interno/curso', id);
+    return this.http.get<Curso>(`${this.url}/interno/curso/${id}`);
   }
 
   list(): Observable<Curso[]> {
-    return this.service.list('interno/curso/l');
+    return this.http.get<Curso[]>(`${this.url}/interno/curso/l`);
   }
 
-  filterByNomeDuracao(filter: CustomFilter): Observable<Curso[]> {
-    return this.service.list(`interno/curso/l?nome=${!!filter.nome ? filter.nome : ''}&duracao=${!!filter.duracao ? filter.duracao : 1}`)
+  filterByNomeDuracao(q: CustomFilter): Observable<Curso[]> {
+    return this.http.get<Curso[]>(`i${this.url}/nterno/curso/l?nome=${!!q.nome ? q.nome : ''}&duracao=${!!q.duracao ? q.duracao : 1}`)
       ;
   }
 
   filterByDuracao(duracao: number): Observable<Curso[]> {
-    return this.service
-      .list(`interno/curso/l?duracao=${!!duracao ? duracao : 1}`);
+    return this.http
+      .get<Curso[]>(`${this.url}/interno/curso/l?duracao=${!!duracao ? duracao : 1}`);
   }
-
 
   save(t: Curso): Observable<Curso> {
     if (t.id) {
-      return this.service.update('interno/curso', t);
+      return this.http.put<Curso>(`${this.url}/interno/curso`, t);
     }
-    return this.service.save('interno/curso', t);
+    return this.http.post<Curso>(`${this.url}/interno/curso`, t);
   }
 
   deleteById(id: number): Observable<void> {
-    return this.service.deleteById('interno/curso', id);
+    return this.http.delete<void>(`${this.url}/interno/curso/${id}`);
   }
 }
