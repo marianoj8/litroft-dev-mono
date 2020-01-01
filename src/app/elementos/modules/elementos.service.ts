42a3596c00ed62ba662f15648123787e435f69d5
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-01 03:04:12 +0100

    Refatoracao completa de elementos.service, impl removida

--- src/app/elementos/modules/elementos.service.ts
+++ src/app/elementos/modules/elementos.service.ts
@@ -1,15 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { EventEmitter, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-import { Elemento } from 'src/app/shared/model/elemento';
 import { CustomFilter } from 'src/app/shared/model/support/custom-filter';
-import { CustomRepository } from 'src/app/shared/repository/custom-repository';
-import { CrudService } from 'src/app/shared/services/crud/crud.service';
+import { Elemento } from 'src/app/shared/model/elemento';
+import { environment } from '../../../environments/environment.prod';
 
 @Injectable({ providedIn: 'root' })
-export class ElementoService implements CustomRepository<Elemento, number> {
+export class ElementoService {
 
-
+  url = environment.API;
   findValueParam = new EventEmitter<string>();
   onChangeContextTitle = new EventEmitter<string>();
   findValueParamFromServer = new EventEmitter<CustomFilter>();
@@ -19,43 +19,37 @@ export class ElementoService implements CustomRepository<Elemento, number> {
   emitOnEditButtonCliked = new EventEmitter<number>();
   emitOnDeleteButtonCliked = new EventEmitter<number>();
 
-  constructor(private service: CrudService<Elemento, number>) { }
+  constructor(private http: HttpClient) { }
 
   getById(id: number): Observable<Elemento> {
-    return this.service.getById('interno/elemento', id);
+    return this.http.get<Elemento>(`${this.url}/interno/elemento/${id}`);
   }
 
   list(): Observable<Elemento[]> {
-    return this.service.list('interno/elemento/l?descricao');
+    return this.http.get<Elemento[]>(`${this.url}/interno/elemento/l?descricao`);
   }
 
   listByParams(p: string, c: number, g: number): Observable<Elemento[]> {
-    return this.service.list(`interno/elemento/l?position=${p}&curso=${c}&grupo=${g}`);
+    return this.http.get<Elemento[]>(`${this.url}/interno/elemento/l?position=${p}&curso=${c}&grupo=${g}`);
   }
-
-  // filterByNomeDuracao(filter: CustomFilter): Observable<Elemento[]> {
-  //   return this.service.list(`elemento/l?nome=${!!filter.nome ? filter.nome : ''}&duracao=${!!filter.duracao ? filter.duracao : 1}`)
-  //     ;
-  // }
 
   filterByDuracao(duracao: number): Observable<Elemento[]> {
-    return this.service
-      .list(`interno/elemento/l?duracao=${!!duracao ? duracao : 1}`);
+    return this.http
+      .get<Elemento[]>(`${this.url}/interno/elemento/l?duracao=${!!duracao ? duracao : 1}`);
   }
-
 
   save(t: Elemento): Observable<Elemento> {
     if (t.id) {
-      return this.service.update('interno/elemento', t);
+      return this.http.put<Elemento>(`${this.url}/interno/elemento`, t);
     }
-    return this.service.save('interno/elemento', t);
+    return this.http.post<Elemento>(`${this.url}/interno/elemento`, t);
   }
 
   saveMulty(t: Elemento[]): Observable<Elemento[]> {
-    return this.service.saveMany('interno/elemento/multy', t);
+    return this.http.post<Elemento[]>(`${this.url}/interno/elemento/multy`, t);
   }
 
   deleteById(id: number): Observable<void> {
-    return this.service.deleteById('interno/elemento', id);
+    return this.http.delete<void>(`${this.url}/interno/elemento/${id}`);
   }
 }
