31d3f861d77333a1826a558f382156dbfa6e2c89
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-01 01:55:33 +0100

    Refatoracao completa de instituto.service, impl removida

--- src/app/institutos/modules/instituto.service.ts
+++ src/app/institutos/modules/instituto.service.ts
@@ -1,18 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { EventEmitter, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-
 import { Curso } from 'src/app/shared/model/curso';
 import { Instituto } from 'src/app/shared/model/instituto';
 import { CustomFilter } from 'src/app/shared/model/support/custom-filter';
-import { CustomRepository } from 'src/app/shared/repository/custom-repository';
-import { CrudService } from 'src/app/shared/services/crud/crud.service';
 import { environment } from 'src/environments/environment';
 
 
 @Injectable({ providedIn: 'root' })
-export class InstitutoService implements CustomRepository<Instituto, number> {
-
+export class InstitutoService {
   findValueParam = new EventEmitter<string>();
   onChangeContextTitle = new EventEmitter<string>();
   findValueParamFromServer = new EventEmitter<CustomFilter>();
@@ -21,40 +17,46 @@ export class InstitutoService implements CustomRepository<Instituto, number> {
   emitOnDetalheButtonCliked = new EventEmitter<number>();
   emitOnEditButtonCliked = new EventEmitter<number>();
   emitOnDeleteButtonCliked = new EventEmitter<number>();
-
+  url = environment.API;
   emitSelectedInstituto = new EventEmitter<Instituto>();
-  constructor(
-    private http: HttpClient,
-    private service: CrudService<Instituto, number>
-  ) {
 
-  }
+  constructor(
+    private http: HttpClient
+  ) {}
 
   getById(id: number): Observable<Instituto> {
-    return this.service.getById('instituto', id);
+    return this.http.get<Instituto>(`${this.url}/instituto/${id}`);
   }
 
   list(): Observable<Instituto[]> {
-    return this.http.get<Instituto[]>(`${environment.API}/instituto`);
+    console.log(this.url);
+    return this.http.get<Instituto[]>(`${this.url}/instituto`);
   }
 
   listCursoByInstituto(id: number): Observable<Curso[]> {
-    return this.http.get<Curso[]>(`${environment.API}/curso/l/${id}`);
+    return this.http.get<Curso[]>(`${this.url}/curso/l/${id}`);
   }
 
-  listFiltered(nome: string, sigla: string): Observable<Instituto[]> {
-    return this.http.get<Instituto[]>(`${environment.API}/instituto?nome=${nome}&sigla=${sigla}`);
+  listFiltered(filterParam: CustomFilter): Observable<Instituto[]> {
+    filterParam = this.filterResolve(filterParam);
+    return this.http.get<Instituto[]>(`${this.url}/instituto?nome=${filterParam.nome}&sigla=${filterParam.sigla}`);
   }
 
   save(t: Instituto): Observable<Instituto> {
     if (t.id) {
-      return this.service.update('admin/instituto', t);
+      return this.http.put<Instituto>('admin/instituto', t);
     }
-    return this.service.save('admin/instituto', t);
+    return this.http.post<Instituto>('admin/instituto', t);
   }
 
   deleteById(id: number): Observable<void> {
-    return this.service.deleteById('admin/instituto', id);
+    return this.http.delete<void>(`admin/instituto/${id}`);
+  }
+
+  private filterResolve(filterParam: CustomFilter): CustomFilter {
+    filterParam.nome = filterParam.nome === undefined ? '' : filterParam.nome;
+    filterParam.sigla = filterParam.sigla === undefined ? '' : filterParam.sigla;
+    return filterParam;
   }
 
 }
