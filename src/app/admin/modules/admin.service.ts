eeceada218531d9bba29ba32613eabd450f2627e
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-01 03:52:44 +0100

    Metodo nao usados foram comentados

--- src/app/admin/modules/admin.service.ts
+++ src/app/admin/modules/admin.service.ts
@@ -3,62 +3,56 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
-
-import { Admin } from 'src/app/shared/model/admin';
-import { CustomFilter } from 'src/app/shared/model/support/custom-filter';
-import { CustomRepository } from 'src/app/shared/repository/custom-repository';
-import { CrudService } from 'src/app/shared/services/crud/crud.service';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AdminService implements CustomRepository<Admin, number> {
+export class AdminService {
 
 
-  url = `${environment.API}/interno/info`;
+  url = environment.API;
   constructor(
-    private service: CrudService<Admin, number>,
     private http: HttpClient,
     public router: Router) { }
 
 
   getUserInfo(): Observable<boolean> {
-    return this.http.get<boolean>(this.url)
+    return this.http.get<boolean>(`${this.url}/interno/info`)
       .pipe(
         take(1),
         //  catchError((err: HttpErrorResponse) => this.errorHandler(err))
       );
   }
 
-  getById(id: number): Observable<Admin> {
-    return this.service.getById('admin/admin', id);
-  }
+  // getById(id: number): Observable<Admin> {
+  //   return this.http.getById('admin/admin', id);
+  // }
 
-  list(): Observable<Admin[]> {
-    return this.service.list('interno/admin/l?duracao=1');
-  }
+  // list(): Observable<Admin[]> {
+  //   return this.http.list('interno/admin/l?duracao=1');
+  // }
 
-  filterByNomeDuracao(filter: CustomFilter): Observable<Admin[]> {
-    return this.service.list(`interno/admin/l?nome=${!!filter.nome ? filter.nome : ''}&duracao=${!!filter.duracao ? filter.duracao : 1}`)
-      ;
-  }
+  // filterByNomeDuracao(filter: CustomFilter): Observable<Admin[]> {
+  //   return this.http.list(`interno/admin/l?nome=${!!filter.nome ? filter.nome : ''}&duracao=${!!filter.duracao ? filter.duracao : 1}`)
+  //     ;
+  // }
 
-  filterByDuracao(duracao: number): Observable<Admin[]> {
-    return this.service
-      .list(`interno/admin/l?duracao=${!!duracao ? duracao : 1}`);
-  }
+  // filterByDuracao(duracao: number): Observable<Admin[]> {
+  //   return this.http
+  //     .list(`interno/admin/l?duracao=${!!duracao ? duracao : 1}`);
+  // }
 
 
-  save(t: Admin): Observable<Admin> {
-    if (t.id) {
-      return this.service.update('interno/admin', t);
-    }
-    return this.service.save('interno/admin', t);
-  }
+  // save(t: Admin): Observable<Admin> {
+  //   if (t.id) {
+  //     return this.http.update('interno/admin', t);
+  //   }
+  //   return this.http.save('interno/admin', t);
+  // }
 
-  deleteById(id: number): Observable<void> {
-    return this.service.deleteById('interno/admin', id);
-  }
+  // deleteById(id: number): Observable<void> {
+  //   return this.http.deleteById('interno/admin', id);
+  // }
 
 }
