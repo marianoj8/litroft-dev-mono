43ca04ce25db49c766ab0a671632958dbaec4460
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-01 02:00:36 +0100

    Refatoracao completa de public.service, impl removida

--- src/app/public/modules/public.service.ts
+++ src/app/public/modules/public.service.ts
@@ -5,11 +5,12 @@ import { take } from 'rxjs/internal/operators/take';
 import { Grupo } from 'src/app/shared/model/grupo';
 import { environment } from 'src/environments/environment';
 
-import { Instituto } from './../../shared/model/instituto';
 import { CustomFilter } from '../../shared/model/support/custom-filter';
+import { Instituto } from './../../shared/model/instituto';
 
 @Injectable({ providedIn: 'root' })
 export class PublicService {
+  url = environment.API;
   inPublicPage = new EventEmitter<boolean>();
   onChangeContextTitle = new EventEmitter<string>();
   onChangeContext = new EventEmitter<boolean>();
@@ -18,24 +19,34 @@ export class PublicService {
   emitInstitutoList = new EventEmitter<Instituto[]>();
   inFilterMonografias = new EventEmitter<CustomFilter>();
   public emitSelectedSchool = new EventEmitter<Instituto>();
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+  ) { }
 
 
-  listByInstitutoId(id: number): Observable<Grupo[]> {
-    return this.http.get<Grupo[]>(`${environment.API}/grupo/l/public/instituto/${id}`)
+  listByInstitutoId(id?: number): Observable<Grupo[]> {
+    return this.http.get<Grupo[]>(`${this.url}/grupo/l/public/instituto/${id}`)
       .pipe(
         take(1),
         //  catchError((err: HttpErrorResponse) => this.errorHandler(err))
       );
   }
 
-  list(descricao: string): Observable<Grupo[]> {
+  list(descricao?: string): Observable<Grupo[]> {
     descricao = descricao === undefined ? '' : descricao;
-    return this.http.get<Grupo[]>(`${environment.API}/grupo/l/public?descricao=${descricao}`)
+    return this.http.get<Grupo[]>(`${this.url}/grupo/l/public?descricao=${descricao}`)
       .pipe(
         take(1),
         //  catchError((err: HttpErrorResponse) => this.errorHandler(err))
       );
   }
 
+  listByDescription(descricao?: string): Observable<Grupo[]> {
+    descricao = descricao === undefined ? '' : descricao;
+    return this.http.get<Grupo[]>(`${this.url}}/grupo/l/public?descricao=${descricao}`)
+      .pipe(
+        take(1),
+        //  catchError((err: HttpErrorResponse) => this.errorHandler(err))
+      );
+  }
 }
