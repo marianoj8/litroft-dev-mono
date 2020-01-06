826a1bbcbfe89cc16a68b2dbb2043f7606b7602a
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-06 01:16:47 +0100

    O metodo canLoad foi implementado no auth.guard.ts

--- src/app/shared/guard/auth.guard.ts
+++ src/app/shared/guard/auth.guard.ts
@@ -1,18 +1,28 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, CanLoad, Route } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/security/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
+
   constructor(private authService: AuthService) { }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return this.verifyAcess();
+  }
 
+  canLoad(
+    route: Route
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.verifyAcess();
+  }
+
+  private verifyAcess(): boolean {
     if (this.authService.loggedIn() && this.authService.isTokenEpired()) {
       return true;
     } else {
@@ -20,4 +30,5 @@ export class AuthGuard implements CanActivate {
       return false;
     }
   }
+
 }
