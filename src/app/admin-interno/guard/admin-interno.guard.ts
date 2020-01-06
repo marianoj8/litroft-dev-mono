1154ce7c7acd12378c9a6eb438ab19e8dd1ebcd2
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-06 01:20:22 +0100

    Metodo canActivateChild foi implementado. metodo verify foi criado

--- src/app/admin-interno/guard/admin-interno.guard.ts
+++ src/app/admin-interno/guard/admin-interno.guard.ts
@@ -8,7 +8,24 @@ import { Location } from '@angular/common';
 @Injectable({
   providedIn: 'root'
 })
-export class AdminInternoGuard {
+export class AdminInternoGuard implements CanActivateChild {
+  constructor(public adminInternoService: AdminInternoService, public route: Router, public location: Location) {
 
+  }
+  canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.verify(this.route);
+  }
+
+
+  private verify(router: Router): boolean {
+    if (localStorage.getItem('acessType') !== 'Admin') {
+      router.navigate(['/denaid']);
+      return false;
+    }
+    return true;
+  }
 
 }
