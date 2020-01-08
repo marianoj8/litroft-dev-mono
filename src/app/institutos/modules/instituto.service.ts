91b932244a072dc870f0e73a2a8b78e5021883e1
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-08 18:04:13 +0100

    Aqruivo app.component.ts alterdo. metodo ngAfterContentChecked() refatorado.

--- src/app/institutos/modules/instituto.service.ts
+++ src/app/institutos/modules/instituto.service.ts
@@ -29,7 +29,7 @@ export class InstitutoService {
   }
 
   list(): Observable<Instituto[]> {
-    console.log(this.url);
+    // console.log(this.url);
     return this.http.get<Instituto[]>(`${this.url}/instituto`);
   }
 
