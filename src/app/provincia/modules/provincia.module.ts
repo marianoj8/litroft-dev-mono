7a65eabd011c70e4b52944f550e450e1b4455b4b
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-15 10:34:49 +0100

    ProvinciaFormComponent adicionado ao escopo de declaracao do modulo Provincia

--- src/app/provincia/modules/provincia.module.ts
+++ src/app/provincia/modules/provincia.module.ts
@@ -5,9 +5,14 @@ import { MaterialModule } from '../../material/material.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { ProvinciaRoutingModule } from './provincia-routing.module';
 import { ProvinciaListComponent } from '../provincia-list/provincia-list.component';
+import { ProvinciaFormComponent } from '../provincia-form/provincia-form.component';
 
 @NgModule({
-  declarations: [ProvinciaComponent, ProvinciaListComponent],
+  declarations: [
+    ProvinciaComponent,
+    ProvinciaListComponent,
+    ProvinciaFormComponent
+  ],
   imports: [
     CommonModule,
     ProvinciaRoutingModule,
