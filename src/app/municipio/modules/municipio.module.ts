1cec8c39b8e4b2d7167e0d6a2b888cb4d158b4b1
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-15 22:48:48 +0100

    MunicipioFormComponent foi declarado no modulo de municipio

--- src/app/municipio/modules/municipio.module.ts
+++ src/app/municipio/modules/municipio.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { MunicipioRoutingModule } from './municipio-routing.module';
 import { MunicipioComponent } from '../municipio.component';
 import { MunicipioListComponent } from '../municipio-list/municipio-list.component';
+import { MunicipioFormComponent } from '../municipio-form/municipio-form.component';
 
 @NgModule({
   declarations: [
     MunicipioComponent,
-    MunicipioListComponent
+    MunicipioListComponent,
+    MunicipioFormComponent
   ],
   imports: [
     CommonModule,
