6cc05b16933ae58aaa8a9de3a1c90f3be73845cc
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-15 10:19:21 +0100

    Component pai do modulo de proncia foi configurado [html,ts]

--- src/app/provincia/provincia.component.ts
+++ src/app/provincia/provincia.component.ts
@@ -1,4 +1,8 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { CustomFilter } from '../shared/model/support/custom-filter';
+import { Subscription } from 'rxjs';
+import { ProvinciaService } from './modules/provincia.service';
 
 @Component({
   selector: 'app-provincia',
@@ -6,10 +10,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./provincia.component.css']
 })
 export class ProvinciaComponent implements OnInit {
+  state = false;
+  public onChangeContext = false;
+  filtro: CustomFilter = new CustomFilter();
+  private sub: Subscription;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private departamentoSerice: ProvinciaService,
+    private location: Location) {
+    this.departamentoSerice.onChangeContextTitle.emit('Provincias');
   }
 
+  ngOnInit() {
+    this.sub = this.departamentoSerice.onChangeContext.subscribe(
+      context => this.onChangeContext = context
+    );
+
+  }
+
+
+  find(event: KeyboardEvent, value: string) {
+    if (event.key === 'Enter') {
+      this.findFromServer(value);
+    }
+    this.departamentoSerice.findValueParam.emit(value.trim());
+  }
+
+  findFromServer(value: string) {
+    this.filtro.nome = value.trim();
+    this.departamentoSerice.findValueParamFromServer.emit(this.filtro);
+  }
+
+  showAll() {
+    this.filtro.nome = '';
+    this.departamentoSerice.findValueParams.emit(this.filtro);
+  }
+
+  cleanSearchField() {
+    this.findFromServer('');
+  }
+
+  logName(nome) {
+    this.filtro.nome = nome;
+  }
+
+  back() {
+    this.location.back();
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  onFilterSearch() { }
 }
