9a3115c3a871e5b398613eec794ccd328d242a19
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-15 22:46:27 +0100

    Component de listagem de municipio foi criado

---
+++ src/app/municipio/municipio-list/municipio-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-municipio-list',
+  templateUrl: './municipio-list.component.html',
+  styleUrls: ['./municipio-list.component.css']
+})
+export class MunicipioListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
