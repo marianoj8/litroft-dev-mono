5f91a5bfc3e49d6035261af93dc55bbce7c3a6c4
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-15 22:55:29 +0100

    path: 'rota de edicao de municipio foi adicionado edit/:id' com component MunicipioFormComponent

--- src/app/municipio/modules/municipio-routing.module.ts
+++ src/app/municipio/modules/municipio-routing.module.ts
@@ -17,6 +17,10 @@ const routes: Routes = [
       {
         path: 'add',
         component: MunicipioFormComponent
+      },
+      {
+        path: 'edit/:id',
+        component: MunicipioFormComponent
       }
     ]
   }
