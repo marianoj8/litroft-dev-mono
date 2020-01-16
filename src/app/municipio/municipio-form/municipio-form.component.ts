a711db14cce0c4dd0fd9c88af770dc5fc382e3bf
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-16 10:33:00 +0100

    class ts do component de formulario foi configurado

--- src/app/municipio/municipio-form/municipio-form.component.ts
+++ src/app/municipio/municipio-form/municipio-form.component.ts
@@ -1,4 +1,14 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Municipio } from 'src/app/shared/model/monicipio';
+import { MyErrorStateMatch } from 'src/app/shared/validators/field-validator';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { EventEmitter } from 'events';
+import { Router, ActivatedRoute } from '@angular/router';
+import { MunicipioService } from '../modules/municipio.service';
+import { NotificationService } from 'src/app/shared/services/notification/notification.service';
+import { MatVerticalStepper } from '@angular/material';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-municipio-form',
@@ -7,9 +17,107 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MunicipioFormComponent implements OnInit {
 
-  constructor() { }
+  formGroup01: FormGroup;
+  matcher = new MyErrorStateMatch();
+  showAndHideView: EventEmitter = new EventEmitter();
+  municipio: Municipio = new Municipio();
+  private id = 0;
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private municipioService: MunicipioService,
+    private notificationService: NotificationService,
+    private location: Location
+  ) {
+
+  }
 
   ngOnInit() {
+    this.municipioService.onChangeContext.emit(true);
+    this.initForms();
+
+    if (this.router.url.match('/edit')) {
+      this.activatedRoute.params
+        .subscribe(data => {
+          this.id = data.id;
+        });
+
+      this.municipioService.getById(this.id)
+        .subscribe(data => {
+          this.municipio = data;
+
+          this.formGroup01.patchValue({
+            nome: this.municipio.nome,
+          });
+        });
+
+    }
+  }
+
+  initForms() {
+    this.formGroup01 = this.formBuilder.group({
+      nome: ['', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(70)]],
+    });
+
+  }
+
+  onSaveButton(stepper: MatVerticalStepper) {
+    this.save(stepper, false);
+  }
+
+
+  onSaveButtonAndList(stepper: MatVerticalStepper) {
+    this.save(stepper, true);
+  }
+
+
+  private save(stepper: MatVerticalStepper, state): void {
+    this.municipio.nome = this.formGroup01.controls.nome.value;
+
+    this.municipioService.save(this.municipio)
+      .subscribe(
+        (data: Municipio) => {
+          if (!!state) {
+            if (this.router.url.match('/edit')) {
+              this.showUpdatedMessage();
+            } else {
+              this.showSavedMessage();
+            }
+            this.back();
+          } else {
+            if (this.router.url.match('/edit')) {
+              this.showUpdatedMessage();
+            } else {
+              this.showSavedMessage();
+            }
+            stepper.reset();
+          }
+        },
+        (err: HttpErrorResponse) => this.showFailerMessage(err)
+      );
+
+  }
+
+  private showFailerMessage(err: HttpErrorResponse): void {
+    this.notificationService
+      .componentErrorMessage(':: ' + err.error.message);
+  }
+
+  private showSavedMessage(): void {
+    this.notificationService.componentSavedSuccessfulMessage();
+  }
+
+  private showUpdatedMessage(): void {
+    this.notificationService.componentUpdatedSuccessfulMessage();
+  }
+
+  back() {
+    this.location.back();
   }
 
 }
