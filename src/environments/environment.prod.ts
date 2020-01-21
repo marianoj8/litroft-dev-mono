c2d73bdc5fb0886ac366a14117a584b14feaad06
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-21 13:01:23 +0100

    Linhas de comentarios removidos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
   API: '/apiv1/litroft/api/v1/rm'
-  // API: '/apiv1/litroft/api/v1/rm'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  API: 'https://litroft-mono-api.herokuapp.com/litroft/api/v1/rm'
-  // API: '/apiv1/litroft/api/v1/rm'
+  API: 'apiv1/litroft/api/v1/rm'
 };
 
 /*
