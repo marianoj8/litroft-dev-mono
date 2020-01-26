a5f9402c2832bad43a2ef833c1655ff8cae1caba
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-26 11:24:31 +0100

    Variaves de ambiente configurado para localhost

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // API: '/apiv1/litroft/api/v1/rm'
-  API: 'https://litroft-mono-api.herokuapp.com/litroft/api/v1/rm'
+  API: '/apiv1/litroft/api/v1/rm'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  // API: 'apiv1/litroft/api/v1/rm'
-  API: 'https://litroft-mono-api.herokuapp.com/litroft/api/v1/rm'
+  API: 'apiv1/litroft/api/v1/rm'
 };
 
 /*
