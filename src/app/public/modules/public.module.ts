e526741eb534dc639f227fa65cf2393c877a46b3
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-26 22:31:38 +0100

    DetalheComponent foi declarado no modulo public

--- src/app/public/modules/public.module.ts
+++ src/app/public/modules/public.module.ts
@@ -8,9 +8,10 @@ import { ListAllComponent } from '../list-all/list-all.component';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
 import { ReadModeComponent } from '../read-mode/read-mode.component';
 import { CustomPipesModule } from 'src/app/shared/custom-pipes/module/custom-pipes.module';
+import { DetalheComponent } from '../read-mode/detalhe/detalhe.component';
 
 @NgModule({
-  declarations: [PublicComponent, ListAllComponent, ReadModeComponent],
+  declarations: [PublicComponent, ListAllComponent, ReadModeComponent, DetalheComponent],
   imports: [
     CommonModule,
     MaterialModule,
