101792527706434c2f346c7d4e000d95f26b982f
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-01-26 22:36:06 +0100

    component para detalhe de monografia foi criado

---
+++ src/app/public/read-mode/detalhe/detalhe.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DetalheComponent } from './detalhe.component';
+
+describe('DetalheComponent', () => {
+  let component: DetalheComponent;
+  let fixture: ComponentFixture<DetalheComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ DetalheComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DetalheComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/public/read-mode/detalhe/detalhe.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhe',
+  templateUrl: './detalhe.component.html',
+  styleUrls: ['./detalhe.component.css']
+})
+export class DetalheComponent implements OnInit {
+
+  step = 0;
+  ngOnInit(): void {
+
+  }
+
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
+  }
+}
+
