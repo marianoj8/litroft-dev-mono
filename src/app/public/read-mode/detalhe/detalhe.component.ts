d543288acdbadd5e7971d942c98be24940158934
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-02-06 09:48:58 +0100

    Metodo fetchElementos(monografia:Monografia) foi criado

--- src/app/public/read-mode/detalhe/detalhe.component.ts
+++ src/app/public/read-mode/detalhe/detalhe.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PublicService } from '../../modules/public.service';
-import { on } from 'cluster';
 import { Subscription, Observable } from 'rxjs';
 import { MonografiaService } from '../../../monografias/modules/monografia.service';
+import { ElementoService } from '../../../elementos/modules/elementos.service';
 import { Monografia } from '../../../shared/model/monografia';
-import { ActivatedRoute, RouterStateSnapshot } from '@angular/router';
+import { Elemento } from 'src/app/shared/model/elemento';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detalhe',
@@ -14,8 +15,10 @@ import { ActivatedRoute, RouterStateSnapshot } from '@angular/router';
 export class DetalheComponent implements OnInit, OnDestroy {
   subscription: Subscription;
   monografia: Monografia;
+  elementos$: Observable<Elemento[]>;
   constructor(
     private publicService: PublicService,
+    private elementoService: ElementoService,
     private monografiaService: MonografiaService,
     private route: ActivatedRoute) {
 
@@ -24,13 +27,24 @@ export class DetalheComponent implements OnInit, OnDestroy {
   step = 0;
   ngOnInit(): void {
     this.monografiaService.getOneById(this.route.snapshot.url[1].path)
-      .subscribe(onValue => this.monografia = onValue);
+      .subscribe(onValue => {
+        this.monografia = onValue;
+        this.fetchElemets(onValue);
+      });
     this.subscription = this.publicService.emitMonoDetalhe.subscribe(d => this.onActiveStyle());
   }
 
   public onActiveStyle(): void {
     const div = window.document.querySelector('div#mainContainer');
     div.classList.toggle('container-active');
+  }
+
+  public fetchElemets(monografia: Monografia) {
+    const grupo: number = monografia.projeto.grupo.id;
+    const position: string = monografia.projeto.grupo.posicao;
+    const curso: number = monografia.projeto.grupo.curso.id;
+    const instituto: number = monografia.projeto.grupo.adminInterno.instituto.id;
+    this.elementos$ = this.elementoService.listByPublicParams(position, curso, grupo, instituto);
   }
 
   setStep(index: number) {
