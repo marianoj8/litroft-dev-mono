c6103895fd865ecf31f986bf9827ca83449ec255
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-02-07 19:21:48 +0100

    populando turmas ao selecionar curso

--- src/app/estudantes/estudante-filter/estudante-filter.component.ts
+++ src/app/estudantes/estudante-filter/estudante-filter.component.ts
@@ -5,6 +5,8 @@ import { Curso } from 'src/app/shared/model/curso';
 import { CursoService } from 'src/app/cursos/modules/curso.service';
 import { CustomFilter } from 'src/app/shared/model/support/custom-filter';
 import { EstudanteService } from '../modules/estudante.service';
+import { TurmaService } from 'src/app/turmas/modules/turma.service';
+import { Turma } from 'src/app/shared/model/turma';
 
 @Component({
   selector: 'app-estudante-filter',
@@ -14,10 +16,13 @@ import { EstudanteService } from '../modules/estudante.service';
 export class EstudanteFilterComponent implements OnInit {
 
   curso = 0;
+  step = 0;
   filter: CustomFilter = new CustomFilter();
   cursos$: Observable<Curso[]>;
+  turma$: Observable<Turma[]>;
 
   constructor(
+    private turmaService: TurmaService,
     private cursoSerice: CursoService,
     private estudanteSerice: EstudanteService,
   ) { }
@@ -28,7 +33,9 @@ export class EstudanteFilterComponent implements OnInit {
 
   onSelectedCurso(curso: Curso) {
     this.filter.curso = curso.nome;
+    this.turma$ = this.turmaService.findAllByCurso(curso.id);
   }
+
   onSelectedSexo(sexo: string) {
     if (sexo.startsWith('M')) {
       this.filter.sexo = 'M';
@@ -37,6 +44,17 @@ export class EstudanteFilterComponent implements OnInit {
     } else {
       this.filter.sexo = '';
     }
+  }
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
   }
 
   onApplyFilter() {
