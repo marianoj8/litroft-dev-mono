b098b0ae501a0e7ddca6241e87ca85c04c6e2ef9
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-02-11 08:49:27 +0100

    Modulos necessarios foram importados

--- src/app/inscricao/modules/inscricao.module.ts
+++ src/app/inscricao/modules/inscricao.module.ts
@@ -1,13 +1,21 @@
-import { InscricaoRoutingModule } from './inscricao-routing.module';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { InscricaoComponent } from './../inscricao.component';
+import { InscricaoRoutingModule } from './inscricao-routing.module';
+import { MaterialModule } from 'src/app/material/material.module';
 
 @NgModule({
   declarations: [
     InscricaoComponent
   ],
-  imports: [InscricaoRoutingModule],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    InscricaoRoutingModule,
+    FormsModule,
+    ReactiveFormsModule],
   exports: [],
   providers: [],
 })
