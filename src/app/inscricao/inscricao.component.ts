341867d1a053ebd554b0d722d6bd6f66a5f0e39e
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-02-11 08:30:04 +0100

    component e module de inscricao foram criados

---
+++ src/app/inscricao/inscricao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inscricao',
+  templateUrl: './inscricao.component.html',
+  styleUrls: ['./inscricao.component.css']
+})
+export class InscricaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
