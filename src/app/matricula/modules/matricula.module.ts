bae9a2f835622241700a81f9b5ca9d652ab75457
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-02-14 21:31:46 +0100

    component MatriculaFormComponent foi declarado

--- src/app/matricula/modules/matricula.module.ts
+++ src/app/matricula/modules/matricula.module.ts
@@ -7,9 +7,14 @@ import { MaterialModule } from 'src/app/material/material.module';
 import { MatriculaRoutingModule } from './matricula-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatriculaOpcaoComponent } from '../matricula-opcao/matricula-opcao.component';
+import { MatriculaFormComponent } from '../matricula-form/matricula-form.component';
 
 @NgModule({
-  declarations: [MatriculaComponent, MatriculaOpcaoComponent],
+  declarations: [
+    MatriculaComponent,
+    MatriculaOpcaoComponent,
+    MatriculaFormComponent
+  ],
   imports: [
     CommonModule,
     MaterialModule,
