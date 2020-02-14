2d1c923d47026616a051ed8739a72ab1529856cf
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-02-14 21:03:34 +0100

    Modulos necessarios foram declarado no escopo de imports:[]

--- src/app/matricula/modules/matricula.module.ts
+++ src/app/matricula/modules/matricula.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 
 import { MatriculaComponent } from './../matricula.component';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from 'src/app/material/material.module';
+import { MatriculaRoutingModule } from './matricula-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [MatriculaComponent],
-  imports: [],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    MatriculaRoutingModule,
+    ReactiveFormsModule
+  ],
   exports: [],
   providers: [],
 })
