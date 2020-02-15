d1dffa10df04f2ea8b79a7c8c21cc77b88d75b77
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-02-15 20:03:50 +0100

    Metodo openForm foi refatorado

--- src/app/matricula/matricula-opcao/matricula-opcao.component.ts
+++ src/app/matricula/matricula-opcao/matricula-opcao.component.ts
@@ -1,4 +1,6 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { MatriculaService } from '../modules/matricula.service';
 
 @Component({
   selector: 'app-matricula-opcao',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MatriculaOpcaoComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private matriculaService: MatriculaService
+  ) { }
 
   ngOnInit() {
   }
 
+  private opnForm(option: number): void {
+    if (option === 0) {
+      this.router.navigate(['from/primario'], { relativeTo: this.activatedRoute });
+    }
+    if (option === 1) {
+      this.router.navigate(['from/ciculo1'], { relativeTo: this.activatedRoute });
+    }
+    if (option === 2) {
+      this.router.navigate(['from/ciculo2'], { relativeTo: this.activatedRoute });
+    }
+  }
 }
