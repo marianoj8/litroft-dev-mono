3019c58dfde798a6be1f19fdb7c312df8e404216
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-02-19 01:08:33 +0100

    Metodo de CRUD de ensino-nivel foram criados

--- src/app/ensino-nivel/modules/ensino-nivel.service.ts
+++ src/app/ensino-nivel/modules/ensino-nivel.service.ts
@@ -1,7 +1,7 @@
+import { EnsinoNivel } from './../../shared/model/ensinoNivel';
 import { Injectable, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Instituto } from 'src/app/shared/model/instituto';
 import { Curso } from 'src/app/shared/model/curso';
 import { CustomFilter } from 'src/app/shared/model/support/custom-filter';
 import { environment } from 'src/environments/environment';
@@ -19,37 +19,37 @@ export class EnsinoNivelService {
     private http: HttpClient
   ) { }
 
-  getById(id: number): Observable<Instituto> {
-    return this.http.get<Instituto>(`${this.url}/instituto/${id}`);
+  getById(id: number): Observable<EnsinoNivel> {
+    return this.http.get<EnsinoNivel>(`${this.url}/ensinonivel/${id}`);
   }
 
-  list(): Observable<Instituto[]> {
-    return this.http.get<Instituto[]>(`${this.url}/instituto`);
+  list(): Observable<EnsinoNivel[]> {
+    return this.http.get<EnsinoNivel[]>(`${this.url}/ensinonivel`);
   }
 
-  listCursoByInstituto(id: number): Observable<Curso[]> {
-    return this.http.get<Curso[]>(`${this.url}/curso/l/instituto/${id}`);
+  listCursoByEnsinoNivel(id: number): Observable<Curso[]> {
+    return this.http.get<Curso[]>(`${this.url}/curso/l/ensinonivel/${id}`);
   }
 
-  listFiltered(filterParam: CustomFilter): Observable<Instituto[]> {
+  listFiltered(filterParam: CustomFilter): Observable<EnsinoNivel[]> {
     filterParam = this.filterResolve(filterParam);
-    return this.http.get<Instituto[]>(`${this.url}/instituto?nome=${filterParam.nome}&sigla=${filterParam.sigla}`);
+    return this.http.get<EnsinoNivel[]>(`${this.url}/ensinonivel?nome=${filterParam.nome}&sigla=${filterParam.sigla}`);
   }
 
-  listFilteredWithNivel(filterParam: CustomFilter): Observable<Instituto[]> {
+  listFilteredWithNivel(filterParam: CustomFilter): Observable<EnsinoNivel[]> {
     filterParam = this.filterResolve(filterParam);
-    return this.http.get<Instituto[]>(`${this.url}/instituto/nivel?nome=${filterParam.nome}&sigla=${filterParam.sigla}&nivel=${filterParam.nivel}`);
+    return this.http.get<EnsinoNivel[]>(`${this.url}/ensinonivel/nivel?nome=${filterParam.nome}&sigla=${filterParam.sigla}&nivel=${filterParam.nivel}`);
   }
 
-  save(t: Instituto): Observable<Instituto> {
+  save(t: EnsinoNivel): Observable<EnsinoNivel> {
     if (t.id) {
-      return this.http.put<Instituto>(`${this.url}/admin/instituto`, t);
+      return this.http.put<EnsinoNivel>(`${this.url}/admin/ensinonivel`, t);
     }
-    return this.http.post<Instituto>(`${this.url}/admin/instituto`, t);
+    return this.http.post<EnsinoNivel>(`${this.url}/admin/ensinonivel`, t);
   }
 
   deleteById(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.url}/admin/instituto/${id}`);
+    return this.http.delete<void>(`${this.url}/admin/ensinonivel/${id}`);
   }
 
   private filterResolve(filterParam: CustomFilter): CustomFilter {
@@ -57,6 +57,7 @@ export class EnsinoNivelService {
     filterParam.descricao = filterParam.descricao === undefined ? '' : filterParam.descricao;
     filterParam.sigla = filterParam.sigla === undefined ? '' : filterParam.sigla;
     filterParam.nivel = filterParam.nivel === undefined ? '' : filterParam.nivel;
+    filterParam.nivelId = filterParam.nivelId === undefined ? 0 : filterParam.nivelId;
     return filterParam;
   }
 
