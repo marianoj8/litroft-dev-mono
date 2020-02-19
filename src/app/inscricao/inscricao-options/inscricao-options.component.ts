fa279e5a446227b3b8f2b86d3d95e4193b44e962
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-02-19 01:35:06 +0100

    inscricao-opcao.component.ts foi refatorado

--- src/app/inscricao/inscricao-options/inscricao-options.component.ts
+++ src/app/inscricao/inscricao-options/inscricao-options.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { InscricaoService } from '../modules/inscricao.service';
 
 @Component({
   selector: 'app-inscricao-options',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InscricaoOptionsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private inscricaoService: InscricaoService
+  ) { }
 
   ngOnInit() {
+    this.inscricaoService.onChangeContext.emit(false);
+    this.inscricaoService.emitSubOption.subscribe(onValue => this.showOptionPage());
+  }
+
+  private showOptionPage(): void {
+    this.router.navigate(['subscription-option'], { relativeTo: this.activatedRoute });
   }
 
 }
