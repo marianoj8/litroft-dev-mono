5ac8a32f8a3450938579f0e4a07cb4cb5ec485ef
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-02-19 01:24:24 +0100

    rotas do modulo de roteamendo de instituto foram refatoradas

--- src/app/institutos/modules/instituto-routing.module.ts
+++ src/app/institutos/modules/instituto-routing.module.ts
@@ -7,6 +7,7 @@ import { InstitutoFormComponent } from '../instituto-form/instituto-form.compone
 import { AuthGuard } from 'src/app/shared/guard/auth.guard';
 import { InstitutoPriveteListComponent } from '../instituto-privete-list/instituto-privete-list.component';
 import { InstitutoDetalheComponent } from '../instituto-detalhe/instituto-detalhe.component';
+import { InstitutoNivelComponent } from '../instituto-nivel/instituto-nivel.component';
 
 const routes: Routes = [
   {
@@ -15,15 +16,47 @@ const routes: Routes = [
     children: [
       {
         path: '',
+        component: InstitutoNivelComponent
+      },
+      {
+        path: 'primario/list',
         component: InstitutoListComponent
       },
       {
-        path: 'private-list',
+        path: 'ciclo1/list',
+        component: InstitutoListComponent
+      },
+      {
+        path: 'ciclo2/list',
+        component: InstitutoListComponent
+      },
+      {
+        path: 'private/list',
         canActivate: [AuthGuard],
         component: InstitutoPriveteListComponent
       },
       {
-        path: 'add',
+        path: 'private/ciclo1/list',
+        canActivate: [AuthGuard],
+        component: InstitutoPriveteListComponent
+      },
+      {
+        path: 'private/ciclo2/list',
+        canActivate: [AuthGuard],
+        component: InstitutoPriveteListComponent
+      },
+      {
+        path: 'add/primario',
+        canActivate: [AuthGuard],
+        component: InstitutoFormComponent
+      },
+      {
+        path: 'add/ciclo1',
+        canActivate: [AuthGuard],
+        component: InstitutoFormComponent
+      },
+      {
+        path: 'add/ciclo2',
         canActivate: [AuthGuard],
         component: InstitutoFormComponent
       },
@@ -33,11 +66,20 @@ const routes: Routes = [
         component: InstitutoDetalheComponent
       },
       {
-        path: 'edit/:id',
+        path: 'edit/primario/:id',
         canActivate: [AuthGuard],
         component: InstitutoFormComponent
-      }
-    ]
+      },
+      {
+        path: 'edit/ciclo1/:id',
+        canActivate: [AuthGuard],
+        component: InstitutoFormComponent
+      },
+      {
+        path: 'edit/ciclo2/:id',
+        canActivate: [AuthGuard],
+        component: InstitutoFormComponent
+      }]
   }
 ];
 
