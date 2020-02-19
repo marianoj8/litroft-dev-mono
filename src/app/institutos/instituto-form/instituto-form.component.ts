96720ccd974f56644c37eb3ea7e0353bc07929d3
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-02-19 01:28:58 +0100

    instituto-form.compoent.ts  foi refatrado com nivelEnsino

--- src/app/institutos/instituto-form/instituto-form.component.ts
+++ src/app/institutos/instituto-form/instituto-form.component.ts
@@ -15,6 +15,9 @@ import { NotificationService } from 'src/app/shared/services/notification/notifi
 
 import { InstitutoService } from '../modules/instituto.service';
 import { AreaFormacao } from './../../shared/model/AreaFormacao';
+import { Periodo } from 'src/app/shared/model/periodo';
+import { PeriodoService } from 'src/app/periodos/modules/periodos.service';
+import { EnsinoNivel } from 'src/app/shared/model/ensinoNivel';
 
 @Component({
   selector: 'app-instituto-form',
@@ -27,14 +30,16 @@ export class InstitutoFormComponent implements OnInit {
   formGroup02: FormGroup;
   formGroup03: FormGroup;
   formGroup04: FormGroup;
-  areaFormacao$: Observable<AreaFormacao>;
+  areaFormacao$: Observable<AreaFormacao[]>;
   locais$: Observable<AreaFormacao>;
+  periodos$: Observable<Periodo[]>;
   areaFormacaoErrorError$ = new Subject<boolean>();
   localErrorError$ = new Subject<boolean>();
   instituto = new Instituto();
   salasCount = [1, 2, 3, 4, 5, 6, 7, 8];
   oficinasCount = [1, 2, 3, 4, 5, 6, 7, 8];
   laboratoriosCount = [1, 2, 3, 4, 5, 6];
+  public nivelEnsino: number;
 
   private id = 0;
 
@@ -44,6 +49,7 @@ export class InstitutoFormComponent implements OnInit {
     private monografiaService: MonografiaService,
     private institutoService: InstitutoService,
     private areaFormacaoService: AreaFormacaoService,
+    private periodoService: PeriodoService,
     private localService: LocalService,
     private notificationService: NotificationService,
     private formBuilder: FormBuilder,
@@ -52,29 +58,84 @@ export class InstitutoFormComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.institutoService.emitShowSearchBar.emit(false);
     this.institutoService.onChangeContext.emit(true);
 
-    this.areaFormacao$ = this.areaFormacaoService.list()
-      .pipe(catchError(err => {
-        this.areaFormacaoErrorError$.next(true);
-        return of(null);
+    if (this.router.url.match('/add/primario')) {
+      this.nivelEnsino = 0;
+      this.initFormsPrimario();
+    }
 
-      }));
+    if (this.router.url.match('/add/ciclo1')) {
+      this.nivelEnsino = 1;
+      this.initFormsPrimario();
+    }
 
+    if (this.router.url.match('/add/ciclo2')) {
+      this.nivelEnsino = 2;
+      this.areaFormacao$ = this.areaFormacaoService.list()
+        .pipe(catchError(err => {
+          this.areaFormacaoErrorError$.next(true);
+          return of(null);
+
+        }));
+
+      this.locais$ = this.localService.list()
+        .pipe(catchError(err => {
+          this.localErrorError$.next(true);
+          return of(null);
+        }));
+
+      this.initFormsIIciclo();
+    }
+
+    this.periodos$ = this.periodoService.list();
     this.locais$ = this.localService.list()
       .pipe(catchError(err => {
         this.localErrorError$.next(true);
         return of(null);
       }));
 
-    this.initForms();
-
-
-    if (this.router.url.match('/edit')) {
+    if (this.router.url.match('/institutos/edit/primario') || this.router.url.match('/institutos/edit/ciclo1')) {
+      this.nivelEnsino = 0;
+      this.initFormsPrimario();
       this.activatedRoute.params
         .subscribe(data => {
           this.id = data.id;
         });
+
+      this.institutoService.getById(this.id)
+        .subscribe(data => {
+          this.instituto = data;
+
+          this.formGroup01.patchValue({
+            nome: this.instituto.nome,
+            sigla: this.instituto.sigla,
+            numero: this.instituto.numero
+          });
+
+          this.formGroup02.patchValue({
+            salas: this.instituto.salas,
+            periodo: this.instituto.periodo.id,
+            local: this.instituto.local.id
+          });
+
+          this.formGroup04.patchValue({
+            sobre: this.instituto.sobreInstituto
+          });
+
+        });
+    }
+
+    if (this.router.url.match('/institutos/edit/ciclo2')) {
+      this.nivelEnsino = 2;
+      this.initFormsIIciclo();
+      this.activatedRoute.params
+        .subscribe(data => {
+          this.id = data.id;
+        });
+
+      this.areaFormacao$ = this.areaFormacaoService.list();
 
       this.institutoService.getById(this.id)
         .subscribe(data => {
@@ -94,6 +155,7 @@ export class InstitutoFormComponent implements OnInit {
 
           this.formGroup03.patchValue({
             areaFormacao: this.instituto.areaFormacao.id,
+            periodo: this.instituto.periodo.id,
             local: this.instituto.local.id
           });
 
@@ -102,12 +164,47 @@ export class InstitutoFormComponent implements OnInit {
           });
 
         });
-
     }
+
 
   }
 
-  initForms() {
+  initFormsPrimario() {
+    this.formGroup01 = this.formBuilder.group({
+      nome: ['', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(80)]],
+
+      sigla: ['', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(10)]],
+
+      numero: ['', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(11)]],
+
+    });
+
+    this.formGroup02 = this.formBuilder.group({
+      salas: [8, [
+        Validators.min(8),
+        Validators.required
+      ]],
+      periodo: [null, Validators.required],
+      local: [null, Validators.required]
+    });
+
+    this.formGroup04 = this.formBuilder.group({
+      sobre: [` A instituição conta com ${this.formGroup02.controls.salas.value}
+      salas de aulas, com capacidade de albergar anualmente 6 mil estudantes, 302 docentes,
+      2 laboratórios e 2 oficinas.`]
+    });
+  }
+
+  private initFormsIIciclo(): void {
     this.formGroup01 = this.formBuilder.group({
       nome: ['', [
         Validators.required,
@@ -137,6 +234,7 @@ export class InstitutoFormComponent implements OnInit {
 
     this.formGroup03 = this.formBuilder.group({
       areaFormacao: [null, Validators.required],
+      periodo: [null, Validators.required],
       local: [null, Validators.required]
     });
 
@@ -162,10 +260,35 @@ export class InstitutoFormComponent implements OnInit {
     this.instituto.sigla = this.formGroup01.controls.sigla.value;
     this.instituto.numero = this.formGroup01.controls.numero.value;
     this.instituto.salas = this.formGroup02.controls.salas.value;
-    this.instituto.laboratorios = this.formGroup02.controls.laboratorios.value;
-    this.instituto.oficinas = this.formGroup02.controls.oficinas.value;
-    this.instituto.areaFormacao = new AreaFormacao(this.formGroup03.controls.areaFormacao.value);
-    this.instituto.local = new Local(this.formGroup03.controls.local.value);
+
+    if (this.nivelEnsino === 0 || this.nivelEnsino === 1) {
+      this.instituto.local = new Local(this.formGroup02.controls.local.value);
+      this.instituto.nivel = new EnsinoNivel(1);
+      this.instituto.periodo = new Periodo(this.formGroup02.controls.periodo.value);
+    }
+
+    if (this.nivelEnsino === 1) {
+      this.instituto.local = new Local(this.formGroup02.controls.local.value);
+      this.instituto.nivel = new EnsinoNivel(2);
+      this.instituto.periodo = new Periodo(this.formGroup02.controls.periodo.value);
+    }
+
+    if (this.nivelEnsino === 2) {
+      this.instituto.local = new Local(this.formGroup03.controls.local.value);
+      this.instituto.nivel = new EnsinoNivel(3);
+      this.instituto.periodo = new Periodo(this.formGroup03.controls.periodo.value);
+    }
+
+    if (this.nivelEnsino === 2) {
+      this.instituto.laboratorios = this.formGroup02.controls.laboratorios.value;
+      this.instituto.oficinas = this.formGroup02.controls.oficinas.value;
+      this.instituto.areaFormacao = new AreaFormacao(this.formGroup03.controls.areaFormacao.value);
+    }
+
+    if (this.nivelEnsino === 2) {
+      this.instituto.local = new Local(this.formGroup03.controls.local.value);
+    }
+
 
     this.institutoService.save(this.instituto)
       .subscribe(
