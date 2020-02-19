f3ea1b9a7f276b6560537f2ce2610890d6310ea0
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-02-19 01:19:31 +0100

    atributo emitSelectedOption foi criado

--- src/app/matricula/modules/matricula.service.ts
+++ src/app/matricula/modules/matricula.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class MatriculaService {
+  emitSelectedOption = new EventEmitter<number>();
 
   constructor(private http: HttpClient) {
 
