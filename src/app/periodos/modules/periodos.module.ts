aea1a28e60a2ee770ace84586eca5b88d2a90d46
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-02-19 01:13:54 +0100

    Modulo de roteamento de periodo foi importado

--- src/app/periodos/modules/periodos.module.ts
+++ src/app/periodos/modules/periodos.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 
 import { PeriodosComponent } from '../../periodos/periodos.component';
 import { CommonModule } from '@angular/common';
+import { PeriodoRoutingModule } from './periodos-routing.module';
 
 @NgModule({
   declarations: [PeriodosComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    PeriodoRoutingModule
   ],
   exports: [],
   providers: [],
 })
-export class PeriodosModule { }
+export class PeriodoModule { }
