178dec2a3bcb36801365c9a31df8d21a11fa60bb
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-02-27 12:11:43 +0100

    Metodo de CRUD matericular foi criado

--- src/app/matricula/modules/matricula.service.ts
+++ src/app/matricula/modules/matricula.service.ts
@@ -1,12 +1,21 @@
+import { take } from 'rxjs/operators';
+import { environment } from './../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable, EventEmitter } from '@angular/core';
+import { Estudante } from 'src/app/shared/model/estudante';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class MatriculaService {
   emitSelectedOption = new EventEmitter<number>();
-
+  url = environment.API;
   constructor(private http: HttpClient) {
 
+  }
+
+  public matericular(estudante: Estudante): Observable<Estudante> {
+    return this.http.post<Estudante>(`${this.url}/matricula`, estudante)
+      .pipe(take(1));
   }
 
 }
