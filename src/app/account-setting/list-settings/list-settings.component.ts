0d7922d07d15c8238f95dbe2feeec1767b8fae25
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-03-13 22:33:48 +0100

    Component list-settings foi criado

---
+++ src/app/account-setting/list-settings/list-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-settings',
+  templateUrl: './list-settings.component.html',
+  styleUrls: ['./list-settings.component.css']
+})
+export class ListSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
