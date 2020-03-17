602bee167d6f35078d63ceff445cedc9184fae48
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-03-17 15:27:01 +0100

    Component confirm-dialog foi refatorado

--- src/app/estudantes/confirm-dialog/confirm-dialog.component.ts
+++ src/app/estudantes/confirm-dialog/confirm-dialog.component.ts
@@ -1,6 +1,10 @@
+import { EstudanteService } from './../modules/estudante.service';
+import { Estudante } from './../../shared/model/estudante';
+import { MatDailogTypeParam } from './../../shared/model/support/mat-dialog-type-param';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { catchError } from 'rxjs/operators';
 import { TurmaService } from './../../turmas/modules/turma.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import { Turma } from 'src/app/shared/model/turma';
 
@@ -12,16 +16,24 @@ import { Turma } from 'src/app/shared/model/turma';
 export class ConfirmDialogComponent implements OnInit {
 
   turmas$: Observable<Turma[]>;
+  private estudante$: Observable<Estudante>;
 
   constructor(
-    private turmaService: TurmaService
+    @Inject(MAT_DIALOG_DATA)
+    public data: MatDailogTypeParam,
+    private turmaService: TurmaService,
+    private estudanteService: EstudanteService
   ) {
 
   }
 
   ngOnInit(): void {
     this.turmas$ = this.turmaService.list();
-    this.turmaService.list().subscribe(e => console.log(e));
+    this.estudante$ = this.estudanteService.getById(this.data.id);
+  }
+
+  public confirm(turma: Turma): void {
+
   }
 
 }
