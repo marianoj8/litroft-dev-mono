27b903aa4d5b7b969ccbbda3cc8aed9e5f69702b
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-03-22 12:57:44 +0100

    metodo listarEscola foi refatorado

--- src/app/institutos/instituto-nivel/instituto-nivel.component.ts
+++ src/app/institutos/instituto-nivel/instituto-nivel.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../shared/services/security/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { InstitutoService } from '../modules/instituto.service';
@@ -12,21 +13,34 @@ export class InstitutoNivelComponent implements OnInit {
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
-    private institutoService: InstitutoService
+    private institutoService: InstitutoService,
+    private authService: AuthService
   ) { }
 
   ngOnInit() {
   }
 
   public listarEscola(option: number): void {
-    if (option === 0) {
-      this.router.navigate(['private/list'], { relativeTo: this.activatedRoute });
-    }
-    if (option === 1) {
-      this.router.navigate(['private/ciclo1/list'], { relativeTo: this.activatedRoute });
-    }
-    if (option === 2) {
-      this.router.navigate(['private/ciclo2/list'], { relativeTo: this.activatedRoute });
+    if (this.authService.loggedIn()) {
+      if (option === 0) {
+        this.router.navigate(['private/list'], { relativeTo: this.activatedRoute });
+      }
+      if (option === 1) {
+        this.router.navigate(['private/ciclo1/list'], { relativeTo: this.activatedRoute });
+      }
+      if (option === 2) {
+        this.router.navigate(['private/ciclo2/list'], { relativeTo: this.activatedRoute });
+      }
+    } else {
+      if (option === 0) {
+        this.router.navigate(['primario/list'], { relativeTo: this.activatedRoute });
+      }
+      if (option === 1) {
+        this.router.navigate(['ciclo1/list'], { relativeTo: this.activatedRoute });
+      }
+      if (option === 2) {
+        this.router.navigate(['ciclo2/list'], { relativeTo: this.activatedRoute });
+      }
     }
   }
 }
