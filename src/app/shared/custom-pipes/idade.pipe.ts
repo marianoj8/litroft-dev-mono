3f7e28fc6103ce3e118430665b649cb405bcc65e
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-03-27 22:31:57 +0100

    Pipe de formatacao de idade refatorado

--- src/app/shared/custom-pipes/idade.pipe.ts
+++ src/app/shared/custom-pipes/idade.pipe.ts
@@ -7,15 +7,15 @@ export class IdadePipe implements PipeTransform {
 
   private data: string[];
   private ano;
-  private idade;
+  private idade: number;
 
-  transform(value: string, ...args: string[]): unknown {
+  transform(value: string, ...args: string[]): number {
 
     this.data = value.split('-');
     this.ano = this.data[0] as unknown as number;
     this.idade = (new Date().getFullYear() - this.ano);
 
-    return `${this.idade} Anos`;
+    return this.idade;
   }
 
 }
