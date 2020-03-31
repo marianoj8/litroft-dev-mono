acbc585a5d6ecefc1a2fdb356a56089baff035d6
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-04-01 00:46:50 +0100

    Metodo ngOnInit foi refatorado

--- src/app/institutos/instituto-detalhe/instituto-detalhe.component.ts
+++ src/app/institutos/instituto-detalhe/instituto-detalhe.component.ts
@@ -1,7 +1,10 @@
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { MonografiaService } from 'src/app/monografias/modules/monografia.service';
 import { InstitutoService } from '../modules/instituto.service';
+import { Instituto } from 'src/app/shared/model/instituto';
 
 @Component({
   selector: 'app-instituto-detalhe',
@@ -10,7 +13,10 @@ import { InstitutoService } from '../modules/instituto.service';
 })
 export class InstitutoDetalheComponent implements OnInit {
 
+  public instituto: Instituto;
+
   constructor(
+    private activedRoute: ActivatedRoute,
     private institutoService: InstitutoService,
     private monografiaService: MonografiaService,
     private location: Location) {
@@ -19,6 +25,17 @@ export class InstitutoDetalheComponent implements OnInit {
 
   ngOnInit() {
     this.institutoService.onChangeContext.emit(true);
+    this.institutoService.emitShowSearchBar.emit(false)
+    this.institutoService.getById(this.activedRoute.snapshot.params.id)
+      .subscribe(onValue => {
+        this.instituto = onValue
+        this.institutoService.onChangeContextTitle.emit(this.instituto.nome);
+
+        if(onValue.nome.split(' ').length === 1){
+          this.instituto.nome = `Escola do ${onValue.nivel.descricao} ${onValue.nome}`;
+        }
+
+      });
   }
 
   back() {
