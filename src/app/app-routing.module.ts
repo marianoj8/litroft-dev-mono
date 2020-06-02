7e2f1f09ac0aa7ac2b6e23b06cde658c550c233e
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-06-02 15:16:20 +0100

    sintax de link alterado Angular v9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { PublicModule } from './public/modules/public.module';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -6,119 +7,118 @@ import { AuthGuard } from './shared/guard/auth.guard';
 import { CustomErrorPageComponent } from './custom-error-page/custom-error-page.component';
 import { AcessDenaidComponent } from './shared/acess-denaid/acess-denaid.component';
 import { AdminResolverGuard } from './admin/guard/AdminResolverGuard.guard';
-import { AdminInternoGuard } from './admin-interno/guard/admin-interno.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'public', pathMatch: 'full' },
   {
     path: 'public',
-    loadChildren: 'src/app/public/modules/public.module#PublicModule'
+    loadChildren: ()=> import('src/app/public/modules/public.module').then((m)=> m.PublicModule)
   },
   {
     path: 'home',
     canActivate: [AuthGuard],
     canLoad: [AuthGuard],
-    loadChildren: 'src/app/home/modules/home.module#HomeModule'
+    loadChildren: ()=> import('src/app/home/modules/home.module').then((m)=>m.HomeModule)
   },
   {
     path: 'monografias',
     canActivate: [AuthGuard],
     canLoad: [AuthGuard],
-    loadChildren: 'src/app/monografias/modules/monografia.module#MonografiaModule'
+    loadChildren: ()=> import('src/app/monografias/modules/monografia.module').then((m)=> m.MonografiaModule)
   },
-  {
-    path: 'institutos',
-    loadChildren: 'src/app/institutos/modules/instituto.module#InstitutoModule'
-  },
+  // {
+  //   path: 'institutos',
+  //   loadChildren: ()=> import('src/app/institutos/modules/instituto.module').then((m)=> m.)
+  // },
   {
     path: 'cursos',
     canActivate: [AuthGuard],
     canLoad: [AuthGuard],
-    loadChildren: 'src/app/cursos/modules/curso.module#CursoModule'
+    loadChildren: ()=> import('src/app/cursos/modules/curso.module').then((m)=>m.CursoModule)
   },
   {
     path: 'projetos',
     canActivate: [AuthGuard],
     canLoad: [AuthGuard],
-    loadChildren: 'src/app/projetos/modules/projeto.module#ProjetoModule'
+    loadChildren: ()=> import('src/app/projetos/modules/projeto.module').then((m)=> m.ProjetoModule)
   },
   {
     path: 'departamentos',
     canActivate: [AuthGuard],
-    loadChildren: 'src/app/departamentos/modules/departamento.module#DepartamentoModule'
+    loadChildren: ()=> import('src/app/departamentos/modules/departamento.module').then((m)=> m.DepartamentoModule)
   },
   {
     path: 'estudantes',
     canActivate: [AuthGuard],
     canLoad: [AuthGuard],
-    loadChildren: 'src/app/estudantes/modules/estudante.module#EstudanteModule'
+    loadChildren: ()=> import('src/app/estudantes/modules/estudante.module').then((m)=>m.EstudanteModule)
   },
   {
     path: 'grupos',
     canActivate: [AuthGuard],
     canLoad: [AuthGuard],
-    loadChildren: 'src/app/grupos/modules/grupo.module#GrupoModule'
+    loadChildren: ()=> import('src/app/grupos/modules/grupo.module').then((m)=>m.GrupoModule)
   },
   {
     path: 'orientadores',
     canActivate: [AuthGuard],
     canLoad: [AuthGuard],
-    loadChildren: 'src/app/orientadores/modules/orientador.module#OrientadorModule'
+    loadChildren: ()=> import('src/app/orientadores/modules/orientador.module').then((m)=>m.OrientadorModule)
   },
-  {
-    path: 'turmas',
-    canActivate: [AuthGuard],
-    canLoad: [AuthGuard],
-    loadChildren: 'src/app/turmas/modules/turma.module#TurmaModule'
-  },
+  // {
+  //   path: 'turmas',
+  //   canActivate: [AuthGuard],
+  //   canLoad: [AuthGuard],
+  //   loadChildren: ()=> import('src/app/turmas/modules/turma.module#TurmaModule'
+  // },
   {
     path: 'especialidades',
     canActivate: [AuthGuard],
     canLoad: [AuthGuard],
-    loadChildren: 'src/app/especialidades/modules/especialidade.module#EspecialidadeModule'
+    loadChildren: ()=> import('src/app/especialidades/modules/especialidade.module').then((m)=>m.EspecialidadeModule)
   },
-  {
-    path: 'locais',
-    canActivate: [AuthGuard],
-    canLoad: [AuthGuard],
-    loadChildren: 'src/app/locals/modules/local.module#LocalModule'
-  },
-  {
-    path: 'municipios',
-    canActivate: [AuthGuard],
-    canLoad: [AuthGuard],
-    loadChildren: 'src/app/municipio/modules/municipio.module#MunicipioModule'
-  },
-  {
-    path: 'provincias',
-    canActivate: [AuthGuard],
-    canLoad: [AuthGuard],
-    loadChildren: 'src/app/provincia/modules/provincia.module#ProvinciaModule'
-  },
-  {
-    path: 'area-formacao',
-    canActivate: [AuthGuard],
-    canLoad: [AuthGuard],
-    loadChildren: 'src/app/area-formacao/modules/area-formacao.module#AreaFormacaoModule'
-  },
+  // {
+  //   path: 'locais',
+  //   canActivate: [AuthGuard],
+  //   canLoad: [AuthGuard],
+  //   loadChildren: ()=> import('src/app/locals/modules/local.module').then((m)=> m.Local)
+  // },
+  // {
+  //   path: 'municipios',
+  //   canActivate: [AuthGuard],
+  //   canLoad: [AuthGuard],
+  //   loadChildren: ()=> import('src/app/municipio/modules/municipio.module#MunicipioModule'
+  // },
+  // {
+  //   path: 'provincias',
+  //   canActivate: [AuthGuard],
+  //   canLoad: [AuthGuard],
+  //   loadChildren: ()=> import('src/app/provincia/modules/provincia.module').then((m)=>m.Prov)
+  // },
+  // {
+  //   path: 'area-formacao',
+  //   canActivate: [AuthGuard],
+  //   canLoad: [AuthGuard],
+  //   loadChildren: ()=> import('src/app/area-formacao/modules/area-formacao.module').then((m)=> m.Are)
+  // },
   {
     path: 'login',
-    loadChildren: 'src/app/login/modules/login.module#LoginModule'
+    loadChildren: ()=> import('src/app/login/modules/login.module').then((m)=> m.LoginModule)
   },
   {
     path: 'adminstration',
     canActivate: [AuthGuard],
     canLoad: [AuthGuard],
     // resolve: [AdminResolverGuard],
-    loadChildren: 'src/app/admin/modules/admin.module#AdminModule',
+    loadChildren: ()=> import('src/app/admin/modules/admin.module').then((m)=> m.AdminModule),
   },
-  {
-    path: 'directores',
-    canActivate: [AuthGuard],
-    canLoad: [AuthGuard],
-    canActivateChild: [AdminInternoGuard],
-    loadChildren: 'src/app/admin-interno/modules/adminInterno.module#AdminInternoModule',
-  },
+  // {
+  //   path: 'directores',
+  //   canActivate: [AuthGuard],
+  //   canLoad: [AuthGuard],
+  //   canActivateChild: [AdminInternoGuard],
+  //   loadChildren: ()=> import('src/app/admin-interno/modules/adminInterno.module').then((m)=> m.Admin),
+  // },
   {
     path: 'denaid',
     component: AcessDenaidComponent,
