a9b00cae3ee1f96c5b6c7aaf0c99a4f1f865a855
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-10-04 08:56:39 +0100

    metodos de CRUD foram refatorados. Passando entityId como parametro

--- src/app/estudantes/modules/estudante.service.ts
+++ src/app/estudantes/modules/estudante.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { EventEmitter, Injectable } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Observable } from 'rxjs';
+import { debounceTime, delay } from 'rxjs/operators';
 
 import { Estudante } from 'src/app/shared/model/estudante';
 import { CustomFilter } from 'src/app/shared/model/support/custom-filter';
@@ -21,88 +22,82 @@ export class EstudanteService {
   emitOnDetalheButtonCliked = new EventEmitter<number>();
   emitOnEditButtonCliked = new EventEmitter<number>();
   emitOnDeleteButtonCliked = new EventEmitter<number>();
-  private entityId;
 
   estudanteTable: MatTableDataSource<Estudante[]>;
   constructor(private http: HttpClient) {
-    this.entityId = localStorage.getItem('entityId');
   }
 
-  getById(id: number): Observable<Estudante> {
-    this.entityId = localStorage.getItem('entityId');
-    return this.http.get<Estudante>(`${this.url}/interno/estudante/${id}?institutoId=${this.entityId}`);
+  getById(id: number, entityId: number): Observable<Estudante> {
+    return this.http.get<Estudante>(`${this.url}/interno/estudante/${id}?institutoId=${entityId}`);
   }
 
-  list(): Observable<Estudante[]> {
-    this.entityId = localStorage.getItem('entityId');
-    return this.http.get<Estudante[]>(`${this.url}/interno/estudante/l?institutoId=${this.entityId}`);
+  list(entityId: number): Observable<Estudante[]> {
+    return this.http.get<Estudante[]>(`${this.url}/interno/estudante/l?institutoId=${entityId}`);
   }
 
-  filterByNomeSexoCurso(query: CustomFilter): Observable<Estudante[]> {
-    this.entityId = localStorage.getItem('entityId');
+  filterByNomeSexoCurso(query: CustomFilter, entityId: number): Observable<Estudante[]> {
     query = this.filterResolve(query);
     return this.http
-      .get<Estudante[]>(`${this.url}/interno/estudante/l?nome=${query.nome}&curso=${query.curso}&sexo=${query.sexo}&turma=${query.turma}&institutoId=${this.entityId}`);
+      .get<Estudante[]>(`${this.url}/interno/estudante/l?nome=${query.nome}&curso=${query.curso}&sexo=${query.sexo}&turma=${query.turma}&institutoId=${entityId}`);
   }
 
-  filterByNomeSexoTurma(query: CustomFilter): Observable<Estudante[]> {
-    this.entityId = localStorage.getItem('entityId');
+  filterByNomeSexoTurma(query: CustomFilter, entityId: number): Observable<Estudante[]> {
     query = this.filterResolve(query);
     return this.http
-      .get<Estudante[]>(`${this.url}/interno/estudante?nome=${query.nome}&sexo=${query.sexo}&turma=${query.turma}&institutoId=${this.entityId}`);
+      .get<Estudante[]>(`${this.url}/interno/estudante?nome=${query.nome}&sexo=${query.sexo}&turma=${query.turma}&institutoId=${entityId}`);
   }
-  filterByNomeSexoCursoPendente(query: CustomFilter): Observable<Estudante[]> {
-    this.entityId = localStorage.getItem('entityId');
+
+  countByCursoAndTurma(query: CustomFilter, entityId: number) {
     query = this.filterResolve(query);
     return this.http
-      .get<Estudante[]>(`${this.url}/interno/estudante/pendente?nome=${query.nome}&curso=${query.curso}&sexo=${query.sexo}&institutoId=${this.entityId}`);
+      .get(`${this.url}/interno/estudante/count?cursoId=${query.cursoId}&classeId=${query.classeId}&turmaId=${query.turmaId}&anoLetivo=${query.anoletivo}&institutoId=${entityId}`)
+      .pipe(delay(1000));
   }
 
-  filterByNomeSexoPendenteNivel(query: CustomFilter): Observable<Estudante[]> {
-    this.entityId = localStorage.getItem('entityId');
+  filterByNomeSexoCursoPendente(query: CustomFilter, entityId: number): Observable<Estudante[]> {
     query = this.filterResolve(query);
     return this.http
-      .get<Estudante[]>(`${this.url}/interno/estudante/pendente/nivelId?nome=${query.nome}&nivelId=${query.nivelId}&sexo=${query.sexo}&institutoId=${this.entityId}`);
+      .get<Estudante[]>(`${this.url}/interno/estudante/pendente?nome=${query.nome}&curso=${query.curso}&sexo=${query.sexo}&institutoId=${entityId}`);
   }
 
-  filterBySexoAndCurso(curso: string, sexo: string): Observable<Estudante[]> {
-    this.entityId = localStorage.getItem('entityId');
-    return this.http
-      .get<Estudante[]>(`${this.url}/interno/estudante/l?curso=${!!curso ? curso : ''}&sexo=${!!sexo ? sexo : ''}&institutoId=${this.entityId}`);
-  }
-
-  filterByAllAtributsEntrada(query: CustomFilter): Observable<Estudante[]> {
-    this.entityId = localStorage.getItem('entityId');
+  filterByNomeSexoPendenteNivel(query: CustomFilter, entityId: number): Observable<Estudante[]> {
     query = this.filterResolve(query);
     return this.http
-      .get<Estudante[]>(`${this.url}/interno/estudante/l/entrada?curso=${query.curso}&turma=${query.turma}&sexo=${query.sexo}&entrada=${query.entrada}&institutoId=${this.entityId}`);
+      .get<Estudante[]>(`${this.url}/interno/estudante/pendente/nivelId?nome=${query.nome}&nivelId=${query.nivelId}&sexo=${query.sexo}&institutoId=${entityId}`);
   }
 
-  filterByAllAtributsFinalista(query: CustomFilter): Observable<Estudante[]> {
-    this.entityId = localStorage.getItem('entityId');
+  filterBySexoAndCurso(curso: string, sexo: string, entityId: number): Observable<Estudante[]> {
+    return this.http
+      .get<Estudante[]>(`${this.url}/interno/estudante/l?curso=${!!curso ? curso : ''}&sexo=${!!sexo ? sexo : ''}&institutoId=${entityId}`);
+  }
+
+  filterByAllAtributsEntrada(query: CustomFilter, entityId: number): Observable<Estudante[]> {
     query = this.filterResolve(query);
     return this.http
-      .get<Estudante[]>(`${this.url}/interno/estudante/l/finalista?curso=${query.curso}&turma=${query.turma}&sexo=${query.sexo}&finalista=${query.finalista}&institutoId=${this.entityId}`);
+      .get<Estudante[]>(`${this.url}/interno/estudante/l/entrada?curso=${query.curso}&turma=${query.turma}&sexo=${query.sexo}&entrada=${query.entrada}&institutoId=${entityId}`);
   }
 
-  filterByNotGrupo(query: CustomFilter): Observable<Estudante[]> {
-    this.entityId = localStorage.getItem('entityId');
+  filterByAllAtributsFinalista(query: CustomFilter, entityId: number): Observable<Estudante[]> {
     query = this.filterResolve(query);
     return this.http
-      .get<Estudante[]>(`${this.url}/interno/estudante/l/notgrup?nome=${query.nome}&curso=${query.curso}&turma=${query.turma}&sexo=${query.sexo}&institutoId=${this.entityId}`);
+      .get<Estudante[]>(`${this.url}/interno/estudante/l/finalista?curso=${query.curso}&turma=${query.turma}&sexo=${query.sexo}&finalista=${query.finalista}&institutoId=${entityId}`);
   }
 
-  filterBySexoAndCursoAndPosition(curso: string, sexo: string, p: string): Observable<Estudante[]> {
-    this.entityId = localStorage.getItem('entityId');
+  filterByNotGrupo(query: CustomFilter, entityId: number): Observable<Estudante[]> {
+    query = this.filterResolve(query);
     return this.http
-      .get<Estudante[]>(`${this.url}/interno/estudante/l?curso=${!!curso ? curso : ''}&sexo=${!!sexo ? sexo : ''}&posicao=${!!p ? p : ''}&institutoId=${this.entityId}`);
+      .get<Estudante[]>(`${this.url}/interno/estudante/l/notgrup?nome=${query.nome}&curso=${query.curso}&turma=${query.turma}&sexo=${query.sexo}&institutoId=${entityId}`);
   }
 
-  filterBySexoAndCursoAngGroup(q: CustomFilter): Observable<Estudante[]> {
-    this.entityId = localStorage.getItem('entityId');
+  filterBySexoAndCursoAndPosition(curso: string, sexo: string, p: string, entityId: number): Observable<Estudante[]> {
+    return this.http
+      .get<Estudante[]>(`${this.url}/interno/estudante/l?curso=${!!curso ? curso : ''}&sexo=${!!sexo ? sexo : ''}&posicao=${!!p ? p : ''}&institutoId=${entityId}`);
+  }
+
+  filterBySexoAndCursoAngGroup(q: CustomFilter, entityId: number): Observable<Estudante[]> {
     q = this.filterResolve(q);
     return this.http
-      .get<Estudante[]>(`${this.url}/interno/estudante/l/g?curso=${q.curso}&sexo=${q.sexo}&isGroup=${q.isGroup}&institutoId=${this.entityId}`);
+      .get<Estudante[]>(`${this.url}/interno/estudante/l/g?curso=${q.curso}&sexo=${q.sexo}&isGroup=${q.isGroup}&institutoId=${entityId}`);
   }
 
   save(t: Estudante): Observable<Estudante> {
