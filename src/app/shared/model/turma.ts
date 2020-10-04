b9efc0307327a22dc916d08f0f537acee7d1c17b
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-10-04 08:59:18 +0100

    class de modelo turma foi refatorado. +2 atributos criados

--- src/app/shared/model/turma.ts
+++ src/app/shared/model/turma.ts
@@ -2,6 +2,7 @@ import { CustomAbstractEntity } from './customEntity';
 import { Curso } from 'src/app/shared/model/curso';
 import { Admin } from './admin';
 import { AdminInterno } from './adminInterno';
+import { Classe } from './classe';
 
 export class Turma implements CustomAbstractEntity {
 
@@ -11,7 +12,9 @@ export class Turma implements CustomAbstractEntity {
   totalModified: number;
   sigla: string;
   curso: Curso;
+  classe: Classe;
   adminInterno: AdminInterno;
+  totalAlunos: number;
 
   constructor(id?: number) {
     this.id = id ? id : null;
