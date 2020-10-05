24099f562310e4447b05689d76063c68686470c3
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-10-05 19:32:19 +0100

    component diciplinas foi criado

--- src/app/diciplinas/diciplinas.component.ts
+++ src/app/diciplinas/diciplinas.component.ts
@@ -1,15 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { CustomFilter } from '../shared/model/support/custom-filter';
+import { DiciplinaService } from './modules/diciplina.service';
 
 @Component({
   selector: 'app-diciplinas',
   templateUrl: './diciplinas.component.html',
   styleUrls: ['./diciplinas.component.css']
 })
-export class DiciplinasComponent implements OnInit {
+export class DiciplinasComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  state = false;
+  public onChangeContext = false;
+  filtro: CustomFilter = new CustomFilter();
+  private sub: Subscription;
 
-  ngOnInit(): void {
+  constructor(
+    private diciplinaService: DiciplinaService,
+    private location: Location) {
+    this.diciplinaService.onChangeContextTitle.emit('Diciplinas');
+  }
+
+  ngOnInit() {
+    this.sub = this.diciplinaService.onChangeContext.subscribe(
+      context => this.onChangeContext = context);
+
+
+  }
+
+
+  find(event: KeyboardEvent, value: string) {
+    if (event.key === 'Enter') {
+      this.findFromServer(value);
+    }
+    this.diciplinaService.findValueParam.emit(value.trim());
+  }
+
+  findFromServer(value: string) {
+    this.filtro.nome = value.trim();
+    this.diciplinaService.findValueParamFromServer.emit(this.filtro);
+  }
+
+  showAll() {
+    this.filtro.nome = '';
+    this.diciplinaService.findValueParams.emit(this.filtro);
+  }
+
+  cleanSearchField() {
+    this.findFromServer('');
+  }
+
+  logName(nome) {
+    this.filtro.nome = nome;
+  }
+
+  back() {
+    this.location.back();
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 }
