413b2644d0a0c76b3f61d6c6d6783456ab2effd8
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-10-05 19:34:52 +0100

    routes de diciplina foi refatorado

--- src/app/diciplinas/modules/diciplina-routing.module.ts
+++ src/app/diciplinas/modules/diciplina-routing.module.ts
@@ -1,13 +1,26 @@
+import { DicipliasListComponent } from './../diciplias-list/diciplias-list.component';
 import { DiciplinasComponent } from './../diciplinas.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from 'src/app/shared/guard/auth.guard';
+import { DiciplinaFormComponent } from '../diciplina-form/diciplina-form.component';
 
 
 const routes: Routes = [
   {
     path: '',
     component: DiciplinasComponent,
-    children: []
+    children: [
+      {
+        path: '',
+        component: DicipliasListComponent
+      },
+      {
+        path: 'add',
+        canActivate: [AuthGuard],
+        component: DiciplinaFormComponent
+      },
+    ]
   }
 ];
 
