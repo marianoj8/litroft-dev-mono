8e3b3c37c826054a6928c0522a74dacfc632b5d1
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-10-15 23:26:06 +0100

    variavel de ambiente de producao foi refatorado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  // API: 'https://litroft-mono-api.herokuapp.com/litroft/api/v1/rm',
+  API: 'https://litroft-mono-api.herokuapp.com/litroft/api/v1/rm',
   LOGIN: 'apiv1/login',
-  API: '/apiv1/litroft/api/v1/rm'
 };
