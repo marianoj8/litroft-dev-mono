6f81944360a7febbfdc4ce3981ca8055002abef3
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-10-18 13:08:38 +0100

    component cooredenacao foi refatorado

--- src/app/coordenacao/coordenacao.component.ts
+++ src/app/coordenacao/coordenacao.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { CustomFilter } from '../shared/model/support/custom-filter';
+import { CoordenacaoService } from './modules/coordenacao.service';
 
 @Component({
   selector: 'app-coordenacao',
   templateUrl: './coordenacao.component.html',
   styleUrls: ['./coordenacao.component.css']
 })
-export class CoordenacaoComponent implements OnInit {
+export class CoordenacaoComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  state = false;
+  public onChangeContext = false;
+  filtro: CustomFilter = new CustomFilter();
+  private sub: Subscription;
 
-  ngOnInit(): void {
+  constructor(
+    private especialidadeService: CoordenacaoService,
+    private location: Location) {
+    this.especialidadeService.onChangeContextTitle.emit('Coordenacaos');
+  }
+
+  ngOnInit() {
+    this.sub = this.especialidadeService.onChangeContext.subscribe(
+      context => this.onChangeContext = context);
+
+
+  }
+
+
+  find(event: KeyboardEvent, value: string) {
+    if (event.key === 'Enter') {
+      this.findFromServer(value);
+    }
+    this.especialidadeService.findValueParam.emit(value.trim());
+  }
+
+  findFromServer(value: string) {
+    this.filtro.nome = value.trim();
+    this.especialidadeService.findValueParamFromServer.emit(this.filtro);
+  }
+
+  showAll() {
+    this.filtro.nome = '';
+    this.especialidadeService.findValueParams.emit(this.filtro);
+  }
+
+  cleanSearchField() {
+    this.findFromServer('');
+  }
+
+  logName(nome) {
+    this.filtro.nome = nome;
+  }
+
+  back() {
+    this.location.back();
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 }
