3593b467c6bfacfe6b75077d5c9f244a31f081e5
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-10-31 09:16:51 +0100

    component de listagem de admin foi refatorado

--- src/app/admin/admin-list/admin-list.component.ts
+++ src/app/admin/admin-list/admin-list.component.ts
@@ -1,15 +1,181 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { PageEvent } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Subject, Subscription, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { MonografiaService } from 'src/app/monografias/modules/monografia.service';
+import { OrientadorService } from 'src/app/orientadores/modules/OrientadorService.service';
+import { DeleteDialogComponent } from 'src/app/shared/delete-dialog/delete-dialog.component';
+import { ErrorLoadingComponent } from 'src/app/shared/error-loading/error-loading.component';
+import { Admin } from 'src/app/shared/model/admin';
+import { CustomFilter } from 'src/app/shared/model/support/custom-filter';
+import { MatDailogTypeParam } from 'src/app/shared/model/support/mat-dialog-type-param';
+import { MoreOptionsDialogComponent } from 'src/app/shared/more-options-dialog/more-options-dialog.component';
+import { NotificationService } from 'src/app/shared/services/notification/notification.service';
 
 @Component({
   selector: 'app-admin-list',
   templateUrl: './admin-list.component.html',
   styleUrls: ['./admin-list.component.css']
 })
-export class AdminListComponent implements OnInit {
+export class AdminListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  pageEvent: PageEvent;
+  dialogParam: MatDailogTypeParam = new MatDailogTypeParam();
+  valueParam = '';
+  filtro: CustomFilter = new CustomFilter();
+  orientadores: MatTableDataSource<Admin>;
+  orientadoresList: Admin[] = [];
+  error$ = new Subject<boolean>();
+  private sub: Subscription;
+
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+
+  displaydColumns: string[] = [
+    'nome',
+    'sobrenome',
+    'sexo',
+    'telefone',
+    'detalhe',
+    'edit',
+    'delete'
+  ];
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    public service: OrientadorService,
+    private notification: NotificationService,
+    private dialogService: MatDialog,
+    private monografiaService: MonografiaService) {
+    this.monografiaService.emitShowAddButton.emit(true);
+  }
 
   ngOnInit() {
+    this.service.onChangeContext.emit(false);
+
+    this.sub = this.service.findValueParams
+      .subscribe(next => this.onRefrash(next));
+
+    this.sub = this.service.findValueParam
+      .subscribe(next => this.orientadores.filter = next);
+
+    this.onRefrash(this.filtro);
+
+    this.sub = this.service.emitOnDetalheButtonCliked.subscribe(
+      (next) => this.detalhe(next));
+
+    this.sub = this.service.emitOnEditButtonCliked.subscribe(
+      (next) => this.edit(next));
+
+    this.sub = this.service.emitOnDeleteButtonCliked.subscribe(
+      (next) => this.openDeleteDialog(next));
+
+    this.sub = this.service.findValueParamFromServer.subscribe(
+      (next: CustomFilter) => this.onFilterFromServer(next));
+
+  }
+
+  onFilterFromServer(data: CustomFilter) {
+    this.sub = this.service.filterByNomeSexoEspecialidade(data)
+      .subscribe(next => this.orientadoresList = next);
+  }
+
+  onRefrash(data?: CustomFilter) {
+    this.sub = this.service.filterBySexoAndEspecialidade(data)
+      .pipe(
+        catchError(err => {
+          this.dialogService.open(ErrorLoadingComponent);
+          this.error$.next(true);
+          return of(null);
+        })
+      )
+      .subscribe(
+        next => {
+          const array = next.map((item: Admin) => {
+            return {
+              ...item
+            };
+          });
+          this.orientadores = new MatTableDataSource(array);
+          this.orientadores.sort = this.sort;
+          this.orientadoresList = this.orientadores.data;
+        });
+  }
+
+  private showErrorMessage() {
+    this.notification.componentLoadingFailedMessage();
+  }
+  private showDeletedMessage() {
+    this.notification.componentDeletetedSuccessfulMessage();
+  }
+
+  add() {
+    this.router.navigate(['add'], { relativeTo: this.activatedRoute });
+  }
+
+  detalhe(id: number) {
+    this.router.navigate(['detalhe', id], { relativeTo: this.activatedRoute });
+  }
+  edit(id: number) {
+    this.router.navigate(['edit', id], { relativeTo: this.activatedRoute });
+  }
+
+
+  openMoreOptionDialog(id: number) {
+
+    this.dialogParam.id = id;
+    this.dialogParam.entityName = 'Admin';
+
+    const dialogRef = this.dialogService.open(
+      MoreOptionsDialogComponent,
+      {
+        data: this.dialogParam,
+        height: '280px',
+        width: '360px'
+      });
+
+    dialogRef.afterClosed().subscribe((result: boolean) => {
+      if (result) {
+        // this.deleteOrientador(admin);
+      }
+
+    });
+  }
+  openDeleteDialog(id: number) {
+
+    const dialogRef = this.dialogService.open(
+      DeleteDialogComponent,
+      {
+        data: id,
+        height: '200px',
+        width: '360px'
+      });
+
+    dialogRef.afterClosed().subscribe((result: boolean) => {
+      if (result) {
+        this.deleteOrientador(id);
+      }
+
+    });
+  }
+
+  deleteOrientador(orientadorId: number) {
+    this.service.deleteById(orientadorId)
+      .subscribe(
+        () => {
+          this.onRefrash(this.filtro);
+          this.showDeletedMessage();
+        },
+        err => this.showErrorMessage()
+      );
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 
 }
