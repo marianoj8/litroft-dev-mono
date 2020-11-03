89f9016f6030f1022f20cb8d5890147f5a5e5ba1
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-11-03 15:12:15 +0100

    component ts de form de coordenador foi refatorado

--- src/app/coordenador/coordenador-form/coordenador-form.component.ts
+++ src/app/coordenador/coordenador-form/coordenador-form.component.ts
@@ -1,4 +1,27 @@
+import { Professor } from './../../shared/model/professor';
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { MatVerticalStepper } from '@angular/material/stepper';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { EventEmitter } from 'events';
+import { Observable, Subject, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Location } from '@angular/common';
+
+import { ClasseService } from 'src/app/classe/modules/classe.service';
+import { CursoService } from 'src/app/cursos/modules/curso.service';
+import { MonografiaService } from 'src/app/monografias/modules/monografia.service';
+import { ErrorLoadingComponent } from 'src/app/shared/error-loading/error-loading.component';
+import { Classe } from 'src/app/shared/model/classe';
+import { Curso } from 'src/app/shared/model/curso';
+import { Coordenador } from 'src/app/shared/model/coordenador';
+import { NotificationService } from 'src/app/shared/services/notification/notification.service';
+import { MyErrorStateMatch } from 'src/app/shared/validators/field-validator';
+import { CoordenadorService } from '../modules/coordenador.service';
+import { ProfessorService } from 'src/app/professores/modules/professor.service';
 
 @Component({
   selector: 'app-coordenador-form',
@@ -7,9 +30,144 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoordenadorFormComponent implements OnInit {
 
-  constructor() { }
+  formGroup01: FormGroup;
+  formGroup02: FormGroup;
+  professores$: Observable<Professor[]>;
+  cursos$: Observable<Curso[]>;
+  classes$: Observable<Classe[]>;
+  coordenadorError$ = new Subject<boolean>();
+  cursoError$ = new Subject<boolean>();
+  private institutoId = Number.parseInt(localStorage.getItem('entityId'), 10);
+  private nivelId = Number.parseInt(localStorage.getItem('nivelId'), 10);
 
-  ngOnInit(): void {
+  matcher = new MyErrorStateMatch();
+  showAndHideView: EventEmitter = new EventEmitter();
+  coordenador: Coordenador = new Coordenador();
+  private curso: Curso = new Curso();
+  private classe: Classe = new Classe();
+  private id = 0;
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private coordenadorService: CoordenadorService,
+    private professorSerice: ProfessorService,
+    private cursoSerice: CursoService,
+    private classeSerice: ClasseService,
+    private notificationService: NotificationService,
+    private dialog: MatDialog,
+    private location: Location,
+    private monografiaService: MonografiaService) {
+    this.monografiaService.emitShowAddButton.emit(true);
+  }
+
+  ngOnInit() {
+    this.coordenadorService.onChangeContext.emit(true);
+    this.initForms();
+
+    this.professores$ = this.professorSerice.list(this.institutoId);
+    this.classes$ = this.classeSerice.listClasseByNivelId(this.nivelId);
+
+    this.cursos$ = this.cursoSerice.list(this.institutoId)
+      .pipe(catchError(err => {
+        // console.log(err);
+        this.dialog.open(ErrorLoadingComponent);
+        this.cursoError$.next(true);
+        return of([]);
+      }));
+
+    if (this.router.url.match('/edit')) {
+      this.activatedRoute.params
+        .subscribe(data => {
+          this.id = data.id;
+        });
+
+      this.coordenadorService.getById(this.id, this.institutoId)
+        .subscribe(data => {
+          this.coordenador = data;
+
+          this.formGroup01.patchValue({
+            professor: this.coordenador.professor,
+            curso: this.coordenador.curso
+          });
+
+          this.formGroup02.patchValue({
+            classe: this.coordenador.classe,
+            anoletivo: this.coordenador.anoLetivo
+          });
+        });
+
+    }
+  }
+
+  initForms() {
+    this.formGroup01 = this.formBuilder.group({
+      professor: [null, Validators.required],
+      curso: [null, Validators.required],
+    });
+
+    this.formGroup02 = this.formBuilder.group({
+      classe: [null, Validators.required],
+      anoletivo: [null, Validators.required]
+    });
+  }
+
+  onSaveButton(stepper: MatVerticalStepper) {
+    this.save(stepper, false);
+  }
+
+  onSaveButtonAndList(stepper: MatVerticalStepper) {
+    this.save(stepper, true);
+  }
+
+  private save(stepper: MatVerticalStepper, state): void {
+
+    this.coordenador.professor = this.formGroup01.controls.professor.value;
+    this.coordenador.curso = this.formGroup01.controls.curso.value;
+    this.coordenador.classe = this.formGroup02.controls.classe.value;
+    this.coordenador.anoLetivo = this.formGroup02.controls.anoletivo.value;
+
+    this.coordenadorService.save(this.coordenador)
+      .subscribe(
+        (data: Coordenador) => {
+          if (!!state) {
+            if (this.router.url.match('/edit')) {
+              this.showUpdatedMessage();
+            } else {
+              this.showSavedMessage();
+            }
+            this.back();
+          } else {
+            if (this.router.url.match('/edit')) {
+              this.showUpdatedMessage();
+            } else {
+              this.showSavedMessage();
+            }
+            stepper.reset();
+          }
+        },
+        (err: HttpErrorResponse) => this.showFailerMessage(err)
+      );
+  }
+
+  private showFailerMessage(err: HttpErrorResponse): void {
+    this.notificationService
+      .componentErrorMessage(':: ' + err.error.message);
+  }
+
+  private showSavedMessage(): void {
+    this.notificationService.componentSavedSuccessfulMessage();
+  }
+
+  private showUpdatedMessage(): void {
+    this.notificationService.componentUpdatedSuccessfulMessage();
+  }
+
+  back() {
+    this.location.back();
   }
 
 }
+
+
