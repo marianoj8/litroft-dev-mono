5df84af8197a9ee6939ed0198bb8f98b0c8f2567
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-11-03 08:36:23 +0100

    FormsModule e ReactiveFormsModule foram adicionados ao modulo

--- src/app/coordenador/modules/coordenador.module.ts
+++ src/app/coordenador/modules/coordenador.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../../material/material.module';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
@@ -15,6 +16,8 @@ import { CoordenadorFormComponent } from '../coordenador-form/coordenador-form.c
   ],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     CoordernadorRoutingModule,
     MaterialModule
   ],
