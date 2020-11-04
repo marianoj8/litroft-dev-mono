265555d7a11b55f9dd4e0ff8eddb79dee9932e66
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-11-04 11:03:32 +0100

    component AnoLetivoComponent foi declarado no modulo

--- src/app/ano-letivo/modules/ano-letivo.module.ts
+++ src/app/ano-letivo/modules/ano-letivo.module.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { AnoLetivoComponent } from './../ano-letivo.component';
+
 @NgModule({
-  declarations: [AnoLetivoModule],
+  declarations: [AnoLetivoComponent],
   imports: [CommonModule],
   exports: []
 })
