f8280d0bca0f41c9457e9b4ca374cf7d74d287bc
Mariano JavaSwing <marianogoncalves8@gmail.com>
2020-11-08 21:01:06 +0100

    metodo ngOnInit foi refatorado

--- src/app/coordenador/coordenador.component.ts
+++ src/app/coordenador/coordenador.component.ts
@@ -1,8 +1,15 @@
+import { AnoLetivo } from './../shared/model/support/AnoLetivo';
+import { Curso } from './../shared/model/curso';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Location } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { CustomFilter } from '../shared/model/support/custom-filter';
 import { CoordenadorService } from './modules/coordenador.service';
+import { Classe } from '../shared/model/classe';
+import { AnoLetivoService } from '../ano-letivo/modules/ano-letivo.service';
+import { ClasseService } from '../classe/modules/classe.service';
+import { CursoService } from '../cursos/modules/curso.service';
 
 @Component({
   selector: 'app-coordendor',
@@ -15,9 +22,18 @@ export class CoordenadorComponent implements OnInit, OnDestroy {
   public onChangeContext = false;
   filtro: CustomFilter = new CustomFilter();
   private sub: Subscription;
+  public formGroupSearch: FormGroup;
+  public cursos: Curso[];
+  public classes: Classe[];
+  public years: AnoLetivo[];
+  private institutoId = Number.parseInt(localStorage.getItem('entityId'), 10);
 
   constructor(
+    private cursoService: CursoService,
+    private classeService: ClasseService,
+    private anoLetivoService: AnoLetivoService,
     private coordenadorService: CoordenadorService,
+    private formBuilder: FormBuilder,
     private location: Location) {
     this.coordenadorService.onChangeContextTitle.emit('Coordenador');
   }
@@ -26,6 +42,16 @@ export class CoordenadorComponent implements OnInit, OnDestroy {
     this.sub = this.coordenadorService.onChangeContext.subscribe(
       context => this.onChangeContext = context);
 
+    this.cursoService.list(this.institutoId).subscribe((e) => this.cursos = e);
+    this.classeService.list().subscribe((e) => this.classes = e);
+    this.anoLetivoService.list('').subscribe((e) => this.years = e);
+
+    this.formGroupSearch = this.formBuilder.group({
+      curso: [],
+      classe: [],
+      estudanteNome: [],
+      anoLetivo: [13]
+    });
 
   }
 
