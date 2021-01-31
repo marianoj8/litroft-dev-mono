ff63562b9c23b2e52495c25893fbb9fddfac8c60
Novidade Rufino <novidade-rufino@hotmail.com>
2021-01-31 18:30:46 +0100

    varivel privada url foi criado com volor da environment.API

--- src/app/users/modules/user.service.ts
+++ src/app/users/modules/user.service.ts
@@ -1,9 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
-    constructor(private http:HttpClient){
-        
-    }
+    private url: string = environment.API;
+
+    constructor(private http:HttpClient){}
+
 }
