c918cc3cab2154eaec222de158b1ef3d24774da2
Novidade Rufino <novidade-rufino@hotmail.com>
2021-01-31 18:32:18 +0100

    metodo de listagem de user 'listUser' foi criado

--- src/app/users/modules/user.service.ts
+++ src/app/users/modules/user.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApplicationUser } from 'src/app/shared/model/application-user';
 import { environment } from 'src/environments/environment';
 
 @Injectable({ providedIn: 'root' })
@@ -7,5 +9,9 @@ export class UserService {
     private url: string = environment.API;
 
     constructor(private http:HttpClient){}
+
+    public listUser(): Observable<ApplicationUser[]> {
+        return this.http.get<ApplicationUser[]>(`${this.url}/users`);
+    }
 
 }
