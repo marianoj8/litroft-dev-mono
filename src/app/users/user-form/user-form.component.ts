a2dda36a6b7d63d212173797897642a3621263b7
Novidade Rufino <novidade-rufino@hotmail.com>
2021-02-16 18:20:10 +0100

    arquivo user--from.component ts foi criado

---
+++ src/app/users/user-form/user-form.component.ts
@@ -0,0 +1,13 @@
+import { OnInit, Component } from '@angular/core';
+
+@Component({
+    selector: 'app-project-form',
+    templateUrl: './user-form.component.html',
+    styleUrls: ['./user-form.component.css']
+})
+export class UserFormComponent implements OnInit{
+    
+    ngOnInit(){
+
+    }
+}
